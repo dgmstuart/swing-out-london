c3a8d5905fef69dfcff4045e84a1226bc5fe9a5d
Duncan Stuart <duncanstuart@duncan-stuarts-macbook.local>
2010-09-02 18:33:10 +0100

    Updated capistrano config files

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,10 +1,16 @@
-set :application, "swingoutlondon"
+set :application, "swingoutlondonlive"
 set :repository,  "swingout@swingoutlondon.co.uk:git_repos/swingoutlondon.git"
 set :domain, "swingoutlondon.co.uk"
 
 set :scm, :git
 set :deploy_via, :remote_cache
+set :use_sudo, false
+set :user, "swingout"
 
-role :web, "domain"
-role :app, "domain"
-role :db,  "domain", :primary => true
+set :deploy_to, "/home/#{user}/#{application}"
+
+role :web, domain
+role :app, domain
+role :db,  domain, :primary => true
+
+
