3575cec2dddb9c4b994c0ba299b70d55025d5220
Duncan Stuart <duncanstuart@unknown-00-1e-c2-af-c5-77.home>
2010-12-25 01:02:25 +0000

    Saving progress to page header

--- app/controllers/website_controller.rb
+++ app/controllers/website_controller.rb
@@ -1,8 +1,13 @@
 class WebsiteController < ApplicationController
+  
+  #require "twitter"
+  
   def index
     @classes = Event.active_classes
     @socials_dates = Event.socials_dates(Date.today + (INITIAL_SOCIALS-1))  
-
+    
+    # @latest_tweet = Twitter.user_timeline("swingoutlondon").first.text
+    @latest_tweet= "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce tempus gravida dapibus. Curabitur eget eros arcu. Phasellus eget odio nullam."
     respond_to do |format|
       format.html # index.html.erb
       format.xml  { render :xml => @events }
