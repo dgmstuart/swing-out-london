d919787468616a98dd341c931bf5d43a8834a2f7
Duncan Stuart <duncanstuart@unknown-00-1e-c2-af-c5-77.home>
2010-12-26 23:31:00 +0000

    Rails 3 base upgrade

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Swingoutlondon::Application.config.secret_token = '101b6c106c28d8a16aa68f3ffb363be12d197620ed75027a9a7f749815281abef41c8a06371c4d25b5c9bfdf95616d76800350a1e10d52c94e22c65135101bb1'
