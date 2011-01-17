ef3f6d454775c64778396363777228253e49be6d
Duncan Stuart <duncanstuart@duncan-stuarts-macbook.local>
2011-01-17 19:43:27 +0000

    Including correct javascripts to make deletion work

--- config/application.rb
+++ config/application.rb
@@ -31,8 +31,9 @@ module Swingoutlondon
     # config.i18n.default_locale = :de
 
     # JavaScript files you want as :defaults (application.js is always included).
-    # config.action_view.javascript_expansions[:defaults] = %w(jquery rails)
-
+    config.action_view.javascript_expansions[:defaults] = %w(prototype rails)
+    config.perform_caching = true
+    
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
 
