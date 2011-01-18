d25cbc2641e94fa653fe8ad3d47b8b245641dd26
Duncan Stuart <duncanstuart@duncan-stuarts-macbook.local>
2011-01-18 03:25:45 +0000

    more time zone fiddling

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,7 @@ module Swingoutlondon
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    config.time_zone = 'UTC +00:00'
+    config.time_zone = 'London'
     
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
