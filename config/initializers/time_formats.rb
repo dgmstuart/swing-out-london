836d3f7fa8e50e3c40e9825d981650a886d27a4e
Duncan Stuart <duncanstuart@duncan-stuarts-macbook.local>
2011-01-20 02:22:22 +0000

    updated time formats in the hope that this will fix the UK date issue...

--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -1,11 +1,21 @@
+Date::DATE_FORMATS[:default] = "%d/%m/%Y"
+
+# TODO: REMOVE
 Date::DATE_FORMATS[:uk_date] = "%d/%m/%Y"
 Date::DATE_FORMATS[:uk_datetime] = "%d/%m/%Y %H:%M"
+#END REMOVE
+
 Date::DATE_FORMATS[:timepart] = "%H:%M"
 Date::DATE_FORMATS[:short_date] = lambda { |date| date.strftime("#{date.day.ordinalize} %b") }
 Date::DATE_FORMATS[:listing_date]= lambda { |date| date.strftime("%A #{date.day.ordinalize} %B") }
 
+#ADD?
+Time::DATE_FORMATS[:uk_datetime] = "%d/%m/%Y %H:%M"
+# TODO: REMOVE
 Time::DATE_FORMATS[:uk_date] = "%d/%m/%Y"
 Time::DATE_FORMATS[:uk_datetime] = "%d/%m/%Y %H:%M"
+#END REMOVE
+
 Time::DATE_FORMATS[:timepart] = "%H:%M"
 Time::DATE_FORMATS[:short_date] = lambda { |date| date.strftime("#{date.day.ordinalize} %b") }
 Time::DATE_FORMATS[:listing_date]= lambda { |date| date.strftime("%A #{date.day.ordinalize} %B") }
