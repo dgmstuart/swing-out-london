507e0fb3a3351ba439528a607bc2cc65d9e6d38e
Duncan Stuart <duncanstuart@duncan-stuarts-macbook.local>
2011-01-20 01:26:47 +0000

    Improving the validation message

--- app/helpers/page_helper.rb
+++ app/helpers/page_helper.rb
@@ -31,4 +31,16 @@ module PageHelper
     end
   end
   
+  # Display a link to validate the markup of the page
+  def valid_statement(page)
+    validator_address = "http://validator.w3.org/check?uri=referer"
+    
+    # Different pages may or may not validate depending on use of external namespaces, so this needs to be reflected in the link:
+    if page=="index"
+      return "#{link_to "Almost valid XHTML", validator_address} (#{link_to "blame Facebook & Twitter", :action => "about", :anchor => "validation_failures"})"
+    else
+      return link_to "Valid XHTML", validator_address
+    end
+  end
+  
 end
