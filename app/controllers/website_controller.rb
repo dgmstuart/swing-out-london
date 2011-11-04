df79555514e671b4b8875585ab0af71ad65fe030
Duncan Stuart <duncanstuart@duncan-stuarts-macbook.local>
2011-11-04 00:37:23 +0000

    Not ready to do caching...

--- app/controllers/website_controller.rb
+++ app/controllers/website_controller.rb
@@ -4,7 +4,7 @@ class WebsiteController < ApplicationController
   require 'twitter'
   
   before_filter :get_updated_times
-  caches_action :index
+  #caches_action :index
   
   def index
     @classes = Event.active_classes
