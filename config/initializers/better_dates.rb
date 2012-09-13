6bf13f97a059aee85151e7dd1685d32d440b15c1
Duncan Stuart <dgmstuart@gmail.com>
2012-09-13 01:59:05 +0100

    Removing old hacky monkeypatch to define to_date: this has been implemented since rails 3

--- config/initializers/better_dates.rb
+++ config/initializers/better_dates.rb
@@ -1,18 +1,3 @@
-class String
-   def to_date
-     begin
-       parsed_date = Date.parse(self)
-     rescue Exception => msg
-       logger.warn "[WARNING]: Bad date found: '#{self}' - ignored"
-       return
-     else
-       # Switch around the day and the month
-       return parsed_date
-      end
-   end
-end
-
-
 class Date
   def self.local_today
     Time.zone.today.to_date
