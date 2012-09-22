3dae7485db9b0c07312f9ae388628790d1a3c98f
Duncan Stuart <dgmstuart@gmail.com>
2012-09-22 18:09:07 +0100

    Made error handling of the tweet better

--- app/helpers/twitter_helper.rb
+++ app/helpers/twitter_helper.rb
@@ -2,50 +2,61 @@ module TwitterHelper
   TWITTER_URL = "http://www.twitter.com/swingoutlondon"
   
   def tweet_message
+    message = failure_message
+    # This should only ever end up as the final message if:
+    # 1. The memcached store is unavailable
+    # 2. api_cache threw an exception
+    # 3. api_cache can't retrieve the tweet for over a day (very unlikely)
+    
     begin
       # Cache tweets for 30 minutes, timeout after 1 second
       message = APICache.get('latest_tweet', :cache => 1800, :timeout => 1) do
         logger.info "[INFO]: Retrieving Twitter message from twitter instead of the cache"
-        get_tweet_message
+        begin
+          get_tweet_message
+        rescue Exception => e
+           # The call to the twitter api fails if it can't reach twitter, so we need to handle this           
+          logger.error "[ERROR]: #{e.class} - Failed to get latest tweet with message '#{e}'"
+          raise APICache::InvalidResponse
+        end
       end
     rescue Dalli::RingError => e
       # API cache uses MemCachier via Dalli. Dalli throws an error if it can't contact the store.
       logger.error "[ERROR]: Dalli::RingError - MemCachier isn't available? #{e}'"
       
       # The store isn't available, so get a fresh message:
-      get_tweet_message
+      begin
+        message = get_tweet_message
+      rescue Exception => e  
+        logger.error "[ERROR]: #{e.class} - Failed to get latest tweet with message '#{e}'"
+        # swallow the error
+      end
     rescue => e
       # Catch-all: so that the site doesn't crash when caching breaks
       logger.error "[ERROR]: #{e.class} - #{e.message}" 
       e.backtrace.each { |line| logger.error line }
       
       # Caching isn't working, so get a fresh message:
-      get_tweet_message
+      begin
+        message = get_tweet_message
+      rescue Exception => e  
+        logger.error "[ERROR]: #{e.class} - Failed to get latest tweet with message '#{e}'"
+        # swallow the error
+      end
     end
     
-    if message.blank?
-      # This should only ever happen if:
-      # 1. The memcached store is unavailable
-      # 2. api_cache threw an exception
-      # 3. api_cache can't retrieve the tweet for over a day (very unlikely)
-      failure_message
-    else
-      message
-    end
+    return message
   end
   
-  def get_tweet_message
+  def 
     begin
-      tweet = Twitter.user_timeline("swingoutlondon").first
-    rescue Exception => e
-       # The call to the twitter api fails if it can't reach twitter, so we need to handle this           
-      logger.error "[ERROR]: #{e.class} - Failed to get latest tweet with message '#{e}'"
-      raise APICache::InvalidResponse
-    end
-    construct_message(tweet)
+      
+    
   end
 
-  def construct_message(tweet)
+  def get_tweet_message
+    tweet = Twitter.user_timeline("swingoutlondon").first
+    
     # tweet is a Hashie::Mash, so acts like an object
     created_date_string = tweet.created_at.to_s(:timepart) + " on " + tweet.created_at.to_s(:short_date)
     
