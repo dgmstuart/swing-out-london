31f5ab34ba3244f6dc07d4b9b2cedbb2a3bceb2e
Duncan Stuart <dgmstuart@gmail.com>
2012-10-01 00:35:58 +0100

    trying to fix problem of css not being found by expiring all assets

--- config/application.rb
+++ config/application.rb
@@ -51,7 +51,7 @@ module Swingoutlondon
     config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.0'
+    config.assets.version = '1.1'
     
     # Heroku requires this to be false
     config.assets.initialize_on_precompile=false
