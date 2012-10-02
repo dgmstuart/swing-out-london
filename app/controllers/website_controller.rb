2c7f6834c53f45554cc6211b101b78ac63987b47
Duncan Stuart <dgmstuart@gmail.com>
2012-10-02 01:39:45 +0100

    Only display the last updated time on the main listings page - otherwise it's confusing because of caching

--- app/controllers/website_controller.rb
+++ app/controllers/website_controller.rb
@@ -3,7 +3,6 @@ class WebsiteController < ApplicationController
   require 'rubygems'
   require 'twitter'
   
-  before_filter :get_updated_times
   before_filter :set_cache_control_on_static_pages, only: [:about,:listings_policy]
   caches_action :index, :layout => false, :expires_in => 1.hour
   
@@ -11,12 +10,14 @@ class WebsiteController < ApplicationController
     # Varnish will cache the page for 3600 seconds = 1 hour:
     response.headers['Cache-Control'] = 'public, max-age=3600'
     
-    @classes = Event.listing_classes.includes(:venue, :organiser, :swing_cancellations)
-    
+    @last_updated_datetime = Event.last_updated_datetime
+    @last_updated_time = Event.last_updated_datetime.to_s(:timepart)
+    @last_updated_date = Event.last_updated_datetime.to_s(:listing_date)
     @today = today
     
     @tweet = Tweet.message
     
+    @classes = Event.listing_classes.includes(:venue, :organiser, :swing_cancellations)
     @socials_dates = Event.socials_dates(@today)
     
     # @ad_type = :square_ads
@@ -26,12 +27,6 @@ class WebsiteController < ApplicationController
   end
   
   private
-
-  def get_updated_times
-    @last_updated_time = Event.last_updated_datetime.to_s(:timepart)
-    @last_updated_date = Event.last_updated_datetime.to_s(:listing_date)
-    @last_updated_datetime = Event.last_updated_datetime
-  end
   
   def set_cache_control_on_static_pages
     # Varnish will cache the page for 43200 seconds = 12 hours:
