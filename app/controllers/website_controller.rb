cdb53e59ba994d6eb069e417bff3aa4ab7152181
Duncan Stuart <dgmstuart@gmail.com>
2012-10-03 02:51:08 +0100

    Disable all caching until I can work out what's going wrong :(

--- app/controllers/website_controller.rb
+++ app/controllers/website_controller.rb
@@ -3,11 +3,11 @@ class WebsiteController < ApplicationController
   require 'twitter'
   
   before_filter :set_cache_control_on_static_pages, only: [:about,:listings_policy]
-  caches_action :index, :layout => true, :expires_in => 1.hour
+  # caches_action :index, :layout => true, :expires_in => 1.hour
   
   def index
     # Varnish will cache the page for 3600 seconds = 1 hour:
-    response.headers['Cache-Control'] = 'public, max-age=3600'
+    # response.headers['Cache-Control'] = 'public, max-age=3600'
     
     @last_updated_datetime = Event.last_updated_datetime
     @last_updated_time = Event.last_updated_datetime.to_s(:timepart)
