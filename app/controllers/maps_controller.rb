3347c0b1866b5c86fd8cdeb2f64c4e55a29d8fd5
Duncan Stuart <dgmstuart@gmail.com>
2012-10-09 23:58:29 +0100

    Action caching on maps. No sweeper (kind of tricky!) but should expire in an hour

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,6 +1,8 @@
 class MapsController < ApplicationController
   layout "map" 
   
+  caches_action :socials, :classes, cache_path: Proc.new { |c| c.params }, layout: true, :expires_in => 1.hour, :race_condition_ttl => 10
+  
   def classes
     # Varnish will cache the page for 3600 seconds = 1 hour:
     response.headers['Cache-Control'] = 'public, max-age=3600'
