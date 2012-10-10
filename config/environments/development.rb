4a64fd7805d82f4ee030a0ac4d0714f5575ba2c5
Duncan Stuart <dgmstuart@gmail.com>
2012-10-11 00:49:41 +0100

    Removing logger from application.rb to stop it logging sql output to the tests (it's in production.rb and development.rb where we need it)

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,5 +34,8 @@ Swingoutlondon::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   config.active_record.auto_explain_threshold_in_seconds = 0.5
+  
+  config.logger = Logger.new(STDOUT)
+  config.log_level = :debug
 end
 
