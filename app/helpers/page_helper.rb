fe4c3820daa4600cc86efe4822fd3a04213d8116
Duncan Stuart <dgmstuart@gmail.com>
2012-11-19 02:33:15 +0000

    Got a iphone-y bottom navbar basically working

--- app/helpers/page_helper.rb
+++ app/helpers/page_helper.rb
@@ -12,11 +12,16 @@ module PageHelper
   end
   
   # Links for navigation
-  def nav_link(name, *args)
+  def nav_link(name, options={}, html_options={})
+    icon_class = html_options.delete(:icon_class)
+    content = ""
+    content += content_tag(:i, nil, :class => icon_class) if icon_class
+    content += name
+    
     # Render the link...
-    link_to_unless_current(name, *args) do 
+    link_to_unless_current(raw(content), options, html_options) do 
       # ...but if the page was current, render a span instead
-      content_tag :span, name, :class => "current"
+      content_tag :span, raw(content), :class => "current"
     end 
   end
   
