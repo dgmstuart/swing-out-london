51a7e6b688670d9d09a417b0538f7902ca39cf38
Duncan Stuart <dgmstuart@gmail.com>
2013-09-05 05:03:38 +0100

    moving secret token into an environment variable

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Swingoutlondon::Application.config.secret_token = '101b6c106c28d8a16aa68f3ffb363be12d197620ed75027a9a7f749815281abef41c8a06371c4d25b5c9bfdf95616d76800350a1e10d52c94e22c65135101bb1'
+Swingoutlondon::Application.config.secret_token = ENV["SWINGOUTLONDON_SECRET_TOKEN"]
