4476e2e9b343b87b0e30785179d2668d16a52b93
Duncan Stuart <dgmstuart@gmail.com>
2014-05-09 15:11:51 +0100

    Removed special case for zeus in the spec helper: Seems like zeus no longer needs to be hacked to play nice with rspec loading of support files

--- custom_plan.rb
+++ custom_plan.rb
@@ -1,14 +1,13 @@
 require 'zeus/rails'
 
 class CustomPlan < Zeus::Rails
-  
+
   def spec(argv=ARGV)
-    Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
     if argv.empty?
       exit RSpec::Core::Runner.run(['spec'])
     else
       exit RSpec::Core::Runner.run(argv)
-    end   
+    end
   end
 end
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,16 +7,9 @@ ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 
-def zeus_running?
-  File.exists? '.zeus.sock'
-end
-
-if !zeus_running?
-  # Requires supporting ruby files with custom matchers and macros, etc,
-  # in spec/support/ and its subdirectories.
-  Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
-end
-
+# Requires supporting ruby files with custom matchers and macros, etc,
+# in spec/support/ and its subdirectories.
+Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
 RSpec.configure do |config|
   # ## Mock Framework
@@ -39,11 +32,11 @@ RSpec.configure do |config|
   # automatically. This will be the default behavior in future versions of
   # rspec-rails.
   config.infer_base_class_for_anonymous_controllers = false
-  
+
   # If any specs have a "focus" tag - only run those specs
   config.treat_symbols_as_metadata_keys_with_true_values = true
   config.filter_run :focus => true
   config.run_all_when_everything_filtered = true
-  
+
   config.extend AuthHelper, :type => :controller
 end
