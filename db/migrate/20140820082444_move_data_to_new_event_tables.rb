a8557ebc2ccf98cd3658d828558df3482fc628e2
Duncan Stuart <dgmstuart@gmail.com>
2014-08-25 16:53:42 +0100

    The migrations now only keep Socials data and include passed events

--- db/migrate/20140820082444_move_data_to_new_event_tables.rb
+++ db/migrate/20140820082444_move_data_to_new_event_tables.rb
@@ -1,4 +1,7 @@
 class MoveDataToNewEventTables < ActiveRecord::Migration
+
+
+
   class EventSeed <  ActiveRecord::Base
     belongs_to :event
     belongs_to :venue
@@ -8,7 +11,7 @@ class MoveDataToNewEventTables < ActiveRecord::Migration
   end
 
   def change
-    Event.all.each do |event|
+    Event.socials.each do |event|
       event_seed = create_event_seed(event)
 
       if event.frequency == 1
@@ -16,22 +19,25 @@ class MoveDataToNewEventTables < ActiveRecord::Migration
         create_event_generator(1, start_date, event, event_seed)
       else
         event.dates.each do |date|
-          create_event_generator(0, date, event, event_seed) if date > Date.today
+          create_event_generator(0, date, event, event_seed)
         end
       end
     end
+
+    non_socials = Event.where(has_class: true, has_social: false)
+    non_socials.destroy_all
 
     change_table :events do |t|
       t.rename :title, :name
 
       t.remove :day
       t.remove :event_type
-      t.remove :venue_id
-      t.remove :frequency
-      t.remove :url
+      t.remove :venue_id            #-> EventSeed
+      t.remove :frequency           #-> EventGenerator
+      t.remove :url                 #-> EventSeed
       t.remove :date_array
       t.remove :cancellation_array
-      t.remove :first_date
+      t.remove :first_date          #-> EventGenerator
       t.remove :last_date
       t.remove :shortname
       t.remove :class_style
