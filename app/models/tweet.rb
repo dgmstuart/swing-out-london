022fe746a5f9f1410fcc34c50b3763c2bd025487
Duncan Stuart <dgmstuart@gmail.com>
2015-10-27 22:12:04 +0000

    Fixing the twitter client

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,4 +1,4 @@
-class Tweet < ActiveRecord::Base    
+class Tweet < ActiveRecord::Base
   def self.message
     begin
       # Cache tweets for 30 minutes, timeout after 1 second, use cached data for up to a day
@@ -9,23 +9,23 @@ class Tweet < ActiveRecord::Base
     rescue Dalli::RingError => e
       # API cache uses MemCachier via Dalli. Dalli throws an error if it can't contact the store.
       logger.error "[ERROR]: Dalli::RingError - MemCachier isn't available? #{e}'"
-      
+
       # The store isn't available, so get a fresh message:
       message = get_message
     rescue APICache::CannotFetch => e
-      logger.error "[ERROR]: Request to Twitter failed AND the cache was expired or empty" 
+      logger.error "[ERROR]: Request to Twitter failed AND the cache was expired or empty"
       # Swallow the error and return nil
     rescue => e
       # Catch-all so that the site doesn't error when the Cache is broken
-      # Should only get here if APICache threw an error - NOT if Twitter threw an error 
-      # Log the error and try 
-      logger.error "[ERROR]: #{e.class} - #{e.message}" 
+      # Should only get here if APICache threw an error - NOT if Twitter threw an error
+      # Log the error and try
+      logger.error "[ERROR]: #{e.class} - #{e.message}"
       e.backtrace.each { |line| logger.error line }
-      
+
       # Assume it's a problem with either the cache or APICache so get a fresh message:
       message = get_message
     end
-    
+
     # This should only ever return nil if:
     # 1. The memcached store is unavailable
     # 2. api_cache threw an exception
@@ -35,12 +35,12 @@ class Tweet < ActiveRecord::Base
   end
 
   private
-  
+
   def self.get_message(error_on_failure=nil)
-    Twitter.user_timeline("swingoutlondon", count: 1).first
-  rescue Exception => e  
+    client.user_timeline("swingoutlondon", count: 1).first
+  rescue Exception => e
     if e.message == "Bad Authentication data"
-      # The app needs to have been registered with twitter and the following environment variables need to have been set: 
+      # The app needs to have been registered with twitter and the following environment variables need to have been set:
       #  - TWITTER_CONSUMER_KEY
       #  - TWITTER_CONSUMER_SECRET
       #  - TWITTER_OAUTH_TOKEN
@@ -52,6 +52,15 @@ class Tweet < ActiveRecord::Base
 
     if error_on_failure
       raise error_on_failure # Tells APICache that it's failed, and to use the cached value
+    end
+  end
+
+  def self.client
+    Twitter::REST::Client.new do |config|
+      config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
+      config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
+      config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
+      config.access_token_secret = ENV["TWITTER_ACCESS_SECRET"]
     end
   end
 end
