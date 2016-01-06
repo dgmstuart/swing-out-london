b8a9c82c98ab2bd18d4a9be06b414374df8de90d
Duncan Stuart <dgmstuart@gmail.com>
2016-01-06 23:29:04 +0000

    stuff

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -4,7 +4,7 @@ DAYNAMES = Date::DAYNAMES.to_a[1..6] << Date::DAYNAMES.to_a[0]
 COMPASS_POINTS = %w( C N S E W NE NW SE SW )
 
 LOGINS={
-  "dgms" => "12fc8b7edab94448559b7ac12ee2b8bb",
+  ENV["SOLDN_USERNAME"] => ENV["SOLDN_PASSWORD_HASH"],
 }
 
 # How many socials should be displayed on the index page?
--- spec/support/macros/auth_helper.rb
+++ spec/support/macros/auth_helper.rb
@@ -1,9 +1,11 @@
 module AuthHelper
   def http_login
+
     before(:each) do
-      user = 'dgms'
-      pw = 'Tatgdsd75'
+      user = 'foo'
+      pw = 'bar'
+      stub_const("LOGINS", { user => Digest::MD5.hexdigest("#{pw}") })
       request.env['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Basic.encode_credentials(user,pw)
     end
-  end  
+  end
 end
