b095fa8680aad4ccdb0d09912203c4f33e6094ee
Duncan Stuart <dgmstuart@gmail.com>
2016-03-26 01:40:16 +0000

    (chore): switch from mandrill to sendgrid
    Mandrill is changing to being a premium service

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,11 +67,12 @@ Swingoutlondon::Application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    :port =>           '587',
-    :address =>        'smtp.mandrillapp.com',
-    :user_name =>      ENV['MANDRILL_USERNAME'],
-    :password =>       ENV['MANDRILL_APIKEY'],
-    :domain =>         'heroku.com',
-    :authentication => :plain
+    :port           => '587',
+    :address        => 'smtp.sendgrid.net',
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :authentication => :plain,
+    :enable_starttls_auto => true
   }
 end
