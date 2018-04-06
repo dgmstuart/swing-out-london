d09acf084dbbafae49ecbbab52e22d9c0d61a0ff
Duncan Stuart <dgmstuart@gmail.com>
2018-04-06 13:07:46 +0100

    (chore) remove outdated line from config
    Honestly don't really understand this:
    https://stackoverflow.com/questions/17699866/how-to-setup-javascript-files-as-defaults-in-rails-4

--- config/application.rb
+++ config/application.rb
@@ -35,9 +35,6 @@ module Swingoutlondon
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-    # JavaScript files you want as :defaults (application.js is always included).
-    config.action_view.javascript_expansions[:defaults] = %w(prototype rails)
-
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
 
