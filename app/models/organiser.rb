5592227365ce9628bf9da4ea5bfa7b97dd9aafa7
Duncan Stuart <dgmstuart@gmail.com>
2018-04-06 13:02:18 +0100

    (chore) remove trailing whitespace

--- app/models/organiser.rb
+++ app/models/organiser.rb
@@ -5,30 +5,30 @@ class Organiser < ActiveRecord::Base
   default_scope -> { order(name: :asc) } #sets default search order
 
   validates_presence_of :name
-  
+
   validates_length_of :shortname, :maximum => 20
   validates_uniqueness_of :shortname, :allow_nil => true, :allow_blank => true
-  
+
   def events
     classes + socials
   end
-  
+
   # Are there any active events associated with this organiser?
   def all_events_out_of_date?
     events.each do |event|
       # not out of date means there is at least one event which has current dates...
       return false if !event.out_of_date
     end
-    
+
     return true
   end
-  
+
   def all_events_nearly_out_of_date?
     events.each do |event|
       # not out of date means there is at least one event which has current dates...
       return false if !event.near_out_of_date
     end
-    
+
     return true
   end
 
--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,9 +1,9 @@
 class Venue < ActiveRecord::Base
   acts_as_gmappable :lat => 'lat', :lng => 'lng', :process_geocoding => false
   geocoded_by :postcode,
-              :latitude => :lat, 
+              :latitude => :lat,
               :longitude => :lng
-  
+
   has_many :events
 
   scope :all_with_classes_listed, -> { where(:id => Event.listing_classes.select("distinct venue_id")) }
@@ -14,9 +14,9 @@ class Venue < ActiveRecord::Base
 
   before_validation do
     if lat.nil? || lng.nil?
-      unless geocode 
-        errors.add :lat, "The address information could not be geocoded. 
-          Please check the address information or manually enter 
+      unless geocode
+        errors.add :lat, "The address information could not be geocoded.
+          Please check the address information or manually enter
           latitude and longitude"
       end
     end
@@ -25,11 +25,11 @@ class Venue < ActiveRecord::Base
 
   UNKNOWN_AREA = "Unknown Area"
   UNKNOWN_POSTCODE = "???"
-  
+
  # TODO: legacy
   def compass_text
     title = case compass
-      when "C" then "Central London" 
+      when "C" then "Central London"
       when "N" then "North London"
       when "S" then "South London"
       when "E" then "East London"
@@ -38,75 +38,75 @@ class Venue < ActiveRecord::Base
       when "NW" then "North West London"
       when "SE" then "South East London"
       when "SW" then "South West London"
-    else 
+    else
       UNKNOWN_AREA
-    end  
+    end
     return title
   end
-  
-  
+
+
   def outward_postcode
     return UNKNOWN_POSTCODE if postcode.nil? || postcode.empty?
-    
+
     # Match the first part of the postcode:
     regexp = /[A-PR-UWYZ0-9][A-HK-Y0-9][AEHMNPRTVXY0-9]?[ABEHMNPRVWXY0-9]?/
     regexp.match(postcode.upcase)[0]
   end
-  
+
   def name_and_area
     name + ' - ' + area
-  end 
-  
+  end
+
   # Are there any active events associated with this venue?
   def all_events_out_of_date?
     events.all?{|e| e.out_of_date}
   end
-  
+
   def all_events_nearly_out_of_date?
     events.all?{|e| e.near_out_of_date}
   end
-  
-  
+
+
   # Map-related methods:
   def position
     [lat,lng] unless lat.nil? || lng.nil?
   end
-  
+
   def coordinates
     "[ #{lat.to_s}, #{lng.to_s} ]"
   end
-  
+
   def self.geocode_all
     bulk_geocode
   end
-  
+
   def self.geocode_all_non_geocoded
     bulk_geocode(non_geocoded)
   end
-  
+
   def self.geocoded
     all.select{ |v| !sv.position.nil?}
   end
-  
+
   def self.non_geocoded
     all.select{ |v| v.position.nil?}
   end
-  
-  
+
+
   def self.bulk_geocode(venuelist=all)
     failed_save = []
     failed_geocode = []
 
     venuelist.each do |venue|
-      
+
       if venue.geocode
         failed_save << venue unless venue.save
       else
         failed_geocode << venue
       end
       sleep 0.05 # need to sleep so that Google doesn't get all overwhelmed
-    end  
- 
+    end
+
     if failed_save.empty? && failed_geocode.empty?
       return true
     else
@@ -114,5 +114,5 @@ class Venue < ActiveRecord::Base
     end
 
   end
-  
+
 end
