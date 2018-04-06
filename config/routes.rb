f5db946cd1f8ccfba8586919efc9a9fce96494b2
Duncan Stuart <dgmstuart@gmail.com>
2018-04-06 13:20:56 +0100

    (fix) match => get in routes
    Rails 4 requires the verb to be specified
    
    http://guides.rubyonrails.org/upgrading_ruby_on_rails.html#action-pack

--- config/routes.rb
+++ config/routes.rb
@@ -19,22 +19,22 @@ Swingoutlondon::Application.routes.draw do
     end
   end
 
-  match 'map/classes/(:day)' => 'maps#classes', as: :map_classes
-  match 'map/socials/(:date)' => 'maps#socials', as: :map_socials
-  match 'map' => 'maps#socials'
-  match 'venue_map_info/:id' => 'maps#venue_map_info', :as => :venue_map_info
+  get 'map/classes/(:day)' => 'maps#classes', as: :map_classes
+  get 'map/socials/(:date)' => 'maps#socials', as: :map_socials
+  get 'map' => 'maps#socials'
+  get 'venue_map_info/:id' => 'maps#venue_map_info', :as => :venue_map_info
 
   # legacy routes:
   get 'map/classes/:day/:venue_id', to: redirect('/map/classes/%{day}?venue_id=%{venue_id}')
   get 'map/socials/:date/:venue_id', to: redirect('/map/socials/%{date}?venue_id=%{venue_id}')
 
-  match 'name_clash' => 'name_clash#index'
-  match 'outdated' => 'outdated#index'
+  get 'name_clash' => 'name_clash#index'
+  get 'outdated' => 'outdated#index'
 
-  match 'about' => 'website#about'
-  match 'listings_policy' => 'website#listings_policy'
+  get 'about' => 'website#about'
+  get 'listings_policy' => 'website#listings_policy'
 
-  match 'apple-touch-icon-precomposed' => 'application#not_found'
-  match 'apple-touch-icon-(:size)-precomposed' => 'application#not_found'
-  match 'apple-app-site-association' => 'application#not_found'
+  get 'apple-touch-icon-precomposed' => 'application#not_found'
+  get 'apple-touch-icon-(:size)-precomposed' => 'application#not_found'
+  get 'apple-app-site-association' => 'application#not_found'
 end
