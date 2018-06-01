f41e222c13345d13036df72834e423a924a98515
Duncan Stuart <dgmstuart@gmail.com>
2018-06-01 12:55:05 +0200

    (fix) "get" in controller tests - deprecation warn
    DEPRECATION WARNING: Using positional arguments in functional tests has
    been deprecated,
    in favor of keyword arguments, and will be removed in Rails 5.1.
    
    Deprecated style:
    get :show, { id: 1 }, nil, { notice: "This is a flash message" }
    
    New keyword style:
    get :show, params: { id: 1 }, flash: { notice: "This is a flash message"
    },
      session: nil # Can safely be omitted.

--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -6,26 +6,27 @@ describe EventsController do
   describe "GET show" do
     it "should assign @event" do
       event = FactoryBot.create(:event)
-      get :show, id: event.to_param
+      get :show, params: { id: event.to_param }
       expect(assigns[:event]).to eq(event)
     end
 
     it "should set a message if there is no taster, class or social" do
       event = FactoryBot.build(:event, has_taster: false, has_class: false, has_social: false)
       event.save(validate: false)
-      get :show, id: event.to_param
+      get :show, params: { id: event.to_param }
       expect(assigns[:warning]).not_to be_blank
     end
 
     it "should set a message if there is a taster but no class or social" do
       event = FactoryBot.build(:event, has_taster: true, has_class: false, has_social: false)
       event.save(validate: false)
-      get :show, id: event.to_param
+      get :show, params: { id: event.to_param }
       expect(assigns[:warning]).not_to be_blank
     end
+
     it "should assign no message if there is a class or social" do
       event = FactoryBot.create(:class)
-      get :show, id: event.to_param
+      get :show, params: { id: event.to_param }
       expect(assigns[:warning]).to be_nil
     end
 
@@ -34,24 +35,25 @@ describe EventsController do
   describe "GET new" do
     context 'when a venue id is provided' do
       before { @venue = FactoryBot.create(:venue, id: 23) }
+
       context 'which matches a venue' do
-        before { get :new, venue_id: 23 }
         it "creates an event at that venue" do
+          get :new, params: { venue_id: 23 }
           expect(assigns(:event).venue).to eq @venue
         end
       end
+
       context "which doesn't match a venue" do
         [
           101011010,
           nil
         ].each do |vid|
-          before { get :new, venue_id: vid }
           it "creates an event with a null venue" do
+            get :new, params: { venue_id: vid }
             expect(assigns(:event).venue).to be_nil
           end
         end
       end
     end
-
   end
 end
--- spec/controllers/maps_controller_spec.rb
+++ spec/controllers/maps_controller_spec.rb
@@ -7,23 +7,24 @@ describe MapsController do
         allow(Venue).to receive(:all_with_classes_listed_on_day)
         allow(Venue).to receive(:all_with_classes_listed)
       end
+
       it "@day should be nil if the url contained no date part" do
         get :classes
         expect(assigns[:day]).to be_nil
       end
 
       it "@day should be a capitalised string if the url contained a day name" do
-        get :classes, day: "tuesday"
+        get :classes, params: { day: "tuesday" }
         expect(assigns[:day]).to eq("Tuesday")
       end
 
       context 'when the url contains a string which is not a day name' do
         it "should redirect to the main classes page" do
-          expect(get :classes, day: "fuseday").to redirect_to('/map/classes')
+          expect(get :classes, params: { day: "fuseday" }).to redirect_to('/map/classes')
         end
 
         it "should show a flash message" do
-          get :classes, day: "fuseday"
+          get :classes, params: { day: "fuseday" }
           expect(flash[:warn]).to eq 'We can only show you classes for days of the week'
         end
       end
@@ -32,22 +33,24 @@ describe MapsController do
         before(:each) do
           allow(controller).to receive(:today).and_return(Date.new(2012,10,11)) # A thursday
         end
+
         it "@day should be today's day name (capitalised) if the url contained 'today'" do
-          get :classes, day: "today"
+          get :classes, params: { day: "today" }
           expect(assigns[:day]).to eq("Thursday")
         end
+
         it "@day should be tomorrow's day name (capitalised) if the url contained 'tomorrow'" do
-          get :classes, day: "tomorrow"
+          get :classes, params: { day: "tomorrow" }
           expect(assigns[:day]).to eq("Friday")
         end
 
         context "the url contained 'yesterday'" do
           it "should redirect to the main classes page" do
-            expect(get :classes, day: "yesterday").to redirect_to('/map/classes')
+            expect(get :classes, params: { day: "yesterday" }).to redirect_to('/map/classes')
           end
 
           it "should show a flash message" do
-            get :classes, day: "fuseday"
+            get :classes, params: { day: "fuseday" }
             expect(flash[:warn]).to eq 'We can only show you classes for days of the week'
           end
         end
@@ -59,23 +62,29 @@ describe MapsController do
         expect(assigns[:map_options][:center_latitude]).to eq(51.5264)
         expect(assigns[:map_options][:center_longitude]).to eq(-0.0878)
       end
+
       after(:each) do
         map_is_centred_on_london
       end
+
       context "and there is a day" do
         before(:each) do
           allow(controller).to receive(:get_day).and_return("Monday")
         end
+
         after(:each) do
-          get :classes, day: "Monday"
+          get :classes, params: { day: "Monday" }
         end
+
         it "should centre map on London (empty array)" do
           allow(Venue).to receive(:all_with_classes_listed_on_day).and_return([])
         end
+
         it "should render an empty map (nil array)" do
           allow(Venue).to receive(:all_with_classes_listed_on_day).and_return(nil)
         end
       end
+
       context "and there is no day" do
         after(:each) do
           get :classes
@@ -145,19 +154,19 @@ describe MapsController do
         end
         it "@date should be a date if that date is in the listing dates" do
           allow(Event).to receive(:listing_dates).and_return([@date])
-          get :socials, date: @date_string
+          get :socials, params: { date: @date_string }
           expect(assigns[:date]).to eq(@date)
         end
 
         context 'when the date is not in the listing dates' do
           it "redirects to the main socials page" do
             allow(Event).to receive(:listing_dates).and_return([])
-            expect(get :socials, date: @date_string).to redirect_to('/map/socials')
+            expect(get :socials, params: { date: @date_string }).to redirect_to('/map/socials')
           end
 
           it 'shows a flash message' do
             allow(Event).to receive(:listing_dates).and_return([])
-            get :socials, date: @date_string
+            get :socials, params: { date: @date_string }
             expect(flash[:warn]).to eq 'We can only show you events for the next 14 days'
           end
         end
@@ -169,21 +178,21 @@ describe MapsController do
           allow(controller).to receive(:today).and_return(@date)
         end
         it "@date should be today's date if the description is 'today'" do
-          get :socials, date: "today"
+          get :socials, params: { date: "today" }
           expect(assigns[:date]).to eq(@date)
         end
         it "@date should be tomorrow's date if the description is 'tomorrow'" do
-          get :socials, date: "tomorrow"
+          get :socials, params: { date: "tomorrow" }
           expect(assigns[:date]).to eq(@date + 1)
         end
 
         context "when the description is 'yesterday'" do
           it "redirects to the main socials page" do
-            expect(get :socials, date: 'yesterday').to redirect_to('/map/socials')
+            expect(get :socials, params: { date: 'yesterday' }).to redirect_to('/map/socials')
           end
 
           it 'shows a flash message' do
-            get :socials, date: 'yesterday'
+            get :socials, params: { date: 'yesterday' }
             expect(flash[:warn]).to eq 'We can only show you events for the next 14 days'
           end
         end
@@ -191,11 +200,11 @@ describe MapsController do
 
       context "when the url string doesn't represent a date" do
         it "redirects to the main socials page" do
-          expect(get :socials, date: "asfasfasf").to redirect_to('/map/socials')
+          expect(get :socials, params: { date: "asfasfasf" }).to redirect_to('/map/socials')
         end
 
         it 'shows a flash message' do
-          get :socials, date: 'asfasfasf'
+          get :socials, params: { date: 'asfasfasf' }
           expect(flash[:warn]).to eq 'We can only show you events for the next 14 days'
         end
       end
@@ -207,9 +216,11 @@ describe MapsController do
         allow(Event).to receive(:socials_dates).and_return([[nil,event]])
         get :socials
       end
+
       it "should set the zoom level to 14" do
         expect(assigns["map_options"]["zoom"]).to eq(14)
       end
+
       it "should disable auto zoom" do
         expect(assigns["map_options"]["auto_zoom"]).to eq(false)
       end
