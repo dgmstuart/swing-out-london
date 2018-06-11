c69647a87b3de54afcf971a2978275f88ed50304
Duncan Stuart <dgmstuart@gmail.com>
2018-06-12 01:36:07 +0200

    (chore) Update app config
    Based on running `rake app:upgrade`

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -15,7 +15,7 @@ Rails.application.configure do
   # Configure public file server for tests with Cache-Control for performance.
   config.public_file_server.enabled = true
   config.public_file_server.headers = {
-    'Cache-Control' => 'public, max-age=3600'
+    'Cache-Control' => "public, max-age=#{1.hour.seconds.to_i}"
   }
 
   # Show full error reports and disable caching.
@@ -39,8 +39,6 @@ Rails.application.configure do
 
   # Raises error for missing translations
   config.action_view.raise_on_missing_translations = true
-
-  config.public_file_server.headers = { 'Cache-Control' => 'public, max-age=3600' }
 
   config.action_mailer.default_url_options = { :host => 'swingoutlondon.co.uk' }
 end
