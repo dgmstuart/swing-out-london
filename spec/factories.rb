7b925326c915331a1ad3904b28f2dabc95e607ba
Duncan Stuart <dgmstuart@gmail.com>
2018-10-20 19:37:19 +0200

    (chore) Upgrade rspec and factory bot
    Also fix a deprecation in factory bot: static values are now deprecated
    so need to be wrapped in a block.

--- spec/factories.rb
+++ spec/factories.rb
@@ -2,40 +2,38 @@ require 'ffaker'
 
 FactoryBot.define do
   factory :event, :aliases => [:social, :intermittent_social] do
-    has_taster false
-    has_class false
-    has_social true
-    event_type 'school'
-    frequency 0
-    day 'monday'
+    has_taster { false }
+    has_class { false }
+    has_social { true }
+    event_type { 'school' }
+    frequency { 0 }
+    day { 'monday' }
     url { FFaker::Internet.http_url }
 
     venue
 
     factory :class, :class => Event do
-      has_class true
-      has_social false
-      frequency 1
+      has_class { true }
+      has_social { false }
+      frequency { 1 }
       # generate classes for different days of the week:
       sequence(:day) { |wd| Date::DAYNAMES[wd%7] }
     end
   end
 
   factory :venue do
-    name      "test_venue"
-    area      "test_area"
-    lat       0.0
-    lng       0.0
-    website   'http://www.example.com'
+    name { "test_venue" }
+    area { "test_area" }
+    lat { 0.0 }
+    lng { 0.0 }
+    website { 'http://www.example.com' }
   end
 
   factory :organiser do
-    name "test_organiser"
+    name { "test_organiser" }
   end
 
   factory :swing_date do
-    date    Date.new
+    date { Date.new }
   end
-
-
 end
