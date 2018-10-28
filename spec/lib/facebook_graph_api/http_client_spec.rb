92ec17ee5465dcfa90b81a36372cb173b82cafbf
Duncan Stuart <dgmstuart@gmail.com>
2018-10-28 19:14:13 +0100

    (feature) Pass appsecret proof with FB API calls
    This is a security measure: all API requests require a token to be sent
    which can verify that the sender knows the app secret:
    
    https://developers.facebook.com/docs/graph-api/securing-requests/#appsecret_proof
    
    https://ruby-doc.org/stdlib-2.5.2/libdoc/openssl/rdoc/OpenSSL/HMAC.html

--- spec/lib/facebook_graph_api/http_client_spec.rb
+++ spec/lib/facebook_graph_api/http_client_spec.rb
@@ -7,16 +7,25 @@ RSpec.describe FacebookGraphApi::HttpClient do
   describe '#delete' do
     it 'makes an http request' do
       stub_request(:delete, 'https://example.com/path')
+        .with(query: { appsecret_proof: 'app-secret-proof-token' })
+      proof_generator = instance_double('FacebookGraphApi::AppsecretProofGenerator')
+      allow(proof_generator).to receive(:generate)
+        .with('super-secret-token')
+        .and_return('app-secret-proof-token')
 
       client = described_class.new(
         base_url: 'https://example.com/',
-        auth_token: 'super-secret-token'
+        auth_token: 'super-secret-token',
+        proof_generator: proof_generator
       )
       client.delete('/path')
 
       expect(
         a_request(:delete, 'https://example.com/path')
-        .with(headers: { 'Authorization' => 'Bearer super-secret-token' })
+        .with(
+          headers: { 'Authorization' => 'Bearer super-secret-token' },
+          query: { appsecret_proof: 'app-secret-proof-token' }
+        )
       ).to have_been_made
     end
 
@@ -31,11 +40,14 @@ RSpec.describe FacebookGraphApi::HttpClient do
           }
         }.to_json
         stub_request(:delete, 'https://example.com/path')
+          .with(query: { appsecret_proof: 'app-secret-proof-token' })
           .to_return(status: 400, body: error_json)
+        proof_generator = instance_double('FacebookGraphApi::AppsecretProofGenerator', generate: 'app-secret-proof-token')
 
         client = described_class.new(
           base_url: 'https://example.com/',
-          auth_token: 'super-secret-token'
+          auth_token: 'super-secret-token',
+          proof_generator: proof_generator
         )
 
         expect { client.delete('/path') }
