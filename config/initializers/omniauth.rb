801fb01c75950aa9f8e64adbfb28bb435e35ebc4
Duncan Stuart <dgmstuart@gmail.com>
2018-10-28 16:09:30 +0100

    (feature) disable oauth login in dev
    I'm not sure if this is a good idea, but getting oauth to work locally
    requires using https locally with a real certificate. This is a simple
    way of bypassing login locally.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,3 +3,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET'], scope: ''
 end
+
+OmniAuth.config.test_mode = true if Rails.env.development?
