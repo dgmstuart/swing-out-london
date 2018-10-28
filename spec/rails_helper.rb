ed4a19becedb88883eed0536254e968ef1968200
Duncan Stuart <dgmstuart@gmail.com>
2018-10-28 03:06:16 +0100

    (chore) remove redundant config from rails-helper
    This is now the default behaviour.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -12,11 +12,6 @@ require 'rspec/rails'
 RSpec.configure do |config|
   config.use_transactional_fixtures = true
 
-  # If true, the base class of anonymous controllers will be inferred
-  # automatically. This will be the default behavior in future versions of
-  # rspec-rails.
-  config.infer_base_class_for_anonymous_controllers = false
-
   config.infer_spec_type_from_file_location!
 
   require File.expand_path('support/controller/auth_helper.rb', __dir__)
