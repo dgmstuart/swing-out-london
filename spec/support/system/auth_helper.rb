6501c74d71eb63b35a51b360faa81a71e2c5c364
Duncan Stuart <dgmstuart@gmail.com>
2018-10-30 15:35:57 +0100

    (feature) realistic auth hash from omniauth
    We want to start getting some information from this so let's set up some
    test helpers to let us do that.

--- spec/support/system/auth_helper.rb
+++ spec/support/system/auth_helper.rb
@@ -1,18 +1,20 @@
 # frozen_string_literal: true
 
+require 'support/system/omniauth_helper'
+
 module System
   module AuthHelper
+    include OmniAuthHelper
     RSpec.configure do |config|
       config.before(:each, type: :system) do
         driven_by :rack_test
+        stub_auth_hash
       end
 
       config.before(:each, type: :system, js: true) do
         driven_by :selenium_chrome_headless
       end
     end
-
-    OmniAuth.config.test_mode = true
 
     def skip_login
       login_session = instance_double(LoginSession, 'Fake login', logged_in?: true)
