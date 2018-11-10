7c1de78685f310a7960e9434b06137611591ad61
Duncan Stuart <dgmstuart@gmail.com>
2018-11-10 14:22:39 +0100

    (refactor) Extract omniauth response to lib
    We want to be able to use this in development as well as test.
    Make sure this can't be run in Production

---
+++ lib/omniauth_test_response_builder.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+require 'faker'
+
+class OmniauthTestResponseBuilder
+  def initialize(hash_builder: OmniAuth::AuthHash, mock_auth_config: OmniAuth.config.mock_auth)
+    @hash_builder = hash_builder
+    @mock_auth_config = mock_auth_config
+  end
+
+  def stub_auth_hash(
+    id: Faker::Number.number(17),
+    name: Faker::Name.name,
+    token: SecureRandom.hex
+  )
+    raise "Can't stub authentication in production" if Rails.env.production?
+
+    auth_hash = facebook_auth_hash(id: id, name: name, token: token)
+    mock_auth_config[:facebook] = auth_hash
+  end
+
+  private
+
+  attr_reader :hash_builder, :mock_auth_config
+
+  def facebook_auth_hash(id:, name:, token:)
+    hash_builder.new(
+      'provider' => 'facebook',
+      'uid' => id,
+      'info' => {
+        'name' => name,
+        'image' => "http://graph.facebook.com/v2.10/#{id}/picture"
+      },
+      'credentials' => {
+        'token' => token,
+        'expires_at' => 1546086985,
+        'expires' => true
+      },
+      'extra' => {
+        'raw_info' => {
+          'name' => name,
+          'id' => id
+        }
+      }
+    )
+  end
+end
