f1d3e942f4bf9c2c31a8b934496407acda7c1cba
Duncan Stuart <dgmstuart@gmail.com>
2018-11-10 20:34:27 +0100

    (feature) log when a user revokes permissions

--- app/services/revoke_login.rb
+++ app/services/revoke_login.rb
@@ -5,19 +5,22 @@ require 'facebook_graph_api/api'
 class RevokeLogin
   def initialize(
     http_client_builder: FacebookGraphApi::HttpClient,
-    api_builder: FacebookGraphApi::Api
+    api_builder: FacebookGraphApi::Api,
+    logger: Rails.logger
   )
     @http_client_builder = http_client_builder
     @api_builder = api_builder
+    @logger = logger
   end
 
   def revoke!(user)
     api_for(user.token).revoke_login(user.auth_id)
+    logger.info("Auth id #{user.auth_id} revoked their login permissions")
   end
 
   private
 
-  attr_reader :http_client_builder, :api_builder
+  attr_reader :http_client_builder, :api_builder, :logger
 
   def api_for(auth_token)
     http_client = http_client_builder.new(auth_token: auth_token)
--- spec/services/revoke_login_spec.rb
+++ spec/services/revoke_login_spec.rb
@@ -10,10 +10,11 @@ RSpec.describe RevokeLogin do
       http_client_builder = class_double('FacebookGraphApi::HttpClient', new: http_client)
       api = instance_double('FacebookGraphApi::Api', revoke_login: double)
       api_builder = class_double('FacebookGraphApi::Api', new: api)
+      logger = instance_double('Logger', info: true)
       auth_token = double
       user = instance_double('LoginSession::User', token: auth_token, auth_id: double)
 
-      service = described_class.new(http_client_builder: http_client_builder, api_builder: api_builder)
+      service = described_class.new(http_client_builder: http_client_builder, api_builder: api_builder, logger: logger)
       service.revoke!(user)
 
       expect(http_client_builder).to have_received(:new).with(auth_token: auth_token)
@@ -24,13 +25,28 @@ RSpec.describe RevokeLogin do
       http_client_builder = class_double('FacebookGraphApi::HttpClient', new: double)
       api = instance_double('FacebookGraphApi::Api', revoke_login: double)
       api_builder = class_double('FacebookGraphApi::Api', new: api)
+      logger = instance_double('Logger', info: true)
       auth_id = double
       user = instance_double('LoginSession::User', token: double, auth_id: auth_id)
 
-      service = described_class.new(http_client_builder: http_client_builder, api_builder: api_builder)
+      service = described_class.new(http_client_builder: http_client_builder, api_builder: api_builder, logger: logger)
       service.revoke!(user)
 
       expect(api).to have_received(:revoke_login).with(auth_id)
+    end
+
+    it 'logs that the user revoked their permissions' do
+      http_client_builder = class_double('FacebookGraphApi::HttpClient', new: double)
+      api = instance_double('FacebookGraphApi::Api', revoke_login: double)
+      api_builder = class_double('FacebookGraphApi::Api', new: api)
+      logger = instance_double('Logger', info: true)
+      user = instance_double('LoginSession::User', token: double, auth_id: 12345678901234567)
+
+      service = described_class.new(http_client_builder: http_client_builder, api_builder: api_builder, logger: logger)
+      service.revoke!(user)
+
+      expect(logger).to have_received(:info)
+        .with('Auth id 12345678901234567 revoked their login permissions')
     end
   end
 end
