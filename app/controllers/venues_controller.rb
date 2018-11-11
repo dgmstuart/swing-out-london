3ffa74bc43c260775a75f8b954da4e1cd8592757
Duncan Stuart <dgmstuart@gmail.com>
2018-11-11 20:30:15 +0100

    (refactor) Remove cruft from VenuesController
    This looks like the default generated controller from aaaaaaaages ago.
    we don't need XML and we don't need those comments.

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -1,88 +1,49 @@
 # frozen_string_literal: true
 
 class VenuesController < CMSBaseController
-  # GET /venues
-  # GET /venues.xml
   def index
     @venues = Venue.order('name ASC').includes(:events)
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render xml: @venues }
-    end
   end
 
-  # GET /venues/1
-  # GET /venues/1.xml
   def show
     @venue = Venue.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render xml: @venue }
-    end
   end
 
-  # GET /venues/new
-  # GET /venues/new.xml
   def new
     @venue = Venue.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render xml: @venue }
-    end
   end
 
-  # GET /venues/1/edit
   def edit
     @venue = Venue.find(params[:id])
   end
 
-  # POST /venues
-  # POST /venues.xml
   def create
     @venue = Venue.new(venue_params)
 
-    respond_to do |format|
-      if @venue.save
-        flash[:notice] = 'Venue was successfully created.'
-        format.html { redirect_to(@venue) }
-        format.xml  { render xml: @venue, status: :created, location: @venue }
-      else
-        format.html { render action: 'new' }
-        format.xml  { render xml: @venue.errors, status: :unprocessable_entity }
-      end
+    if @venue.save
+      flash[:notice] = 'Venue was successfully created.'
+      redirect_to(@venue)
+    else
+      render action: 'new'
     end
   end
 
-  # PUT /venues/1
-  # PUT /venues/1.xml
   def update
     @venue = Venue.find(params[:id])
 
-    respond_to do |format|
-      if @venue.update(venue_params)
-        flash[:notice] = 'Venue was successfully updated.'
-        format.html { redirect_to(@venue) }
-        format.xml  { head :ok }
-      else
-        format.html { render action: 'edit' }
-        format.xml  { render xml: @venue.errors, status: :unprocessable_entity }
-      end
+    if @venue.update(venue_params)
+      flash[:notice] = 'Venue was successfully updated.'
+      redirect_to(@venue)
+    else
+      render action: 'edit'
     end
   end
 
-  # DELETE /venues/1
-  # DELETE /venues/1.xml
   def destroy
     @venue = Venue.find(params[:id])
     @venue.destroy
 
-    respond_to do |format|
-      format.html { redirect_to(venues_url) }
-      format.xml  { head :ok }
-    end
+    redirect_to(venues_url)
   end
 
   private
