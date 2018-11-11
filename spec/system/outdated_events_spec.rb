596212e6bb48c497a9deb030932bb216aa81bd18
Duncan Stuart <dgmstuart@gmail.com>
2018-11-11 23:49:39 +0100

    (fix) Make FactoryBot(:event) more realistic
    This was causing models to show up as dirty when they had no changes,
    since e.g. it would show the casting of nil values to "" as changes.
    
    Notice that we can't easily use Faker for the shortname, since shortname
    only accepts characters, and many of the Faker types include e.g.
    hyphenated words
    
    For some reason this broke one of the specs. I'm baffled as to why it
    worked before (using the url rather than the path) and why it broke now,
    but it was definitely this commit which did it.

--- spec/system/outdated_events_spec.rb
+++ spec/system/outdated_events_spec.rb
@@ -10,16 +10,16 @@ RSpec.describe 'Outdated events' do
   end
 
   it 'when there are outdated and nearly outdated events' do
-    outdated_event = FactoryBot.create(:event, id: 1, frequency: 4, dates: [Date.local_today - 4.weeks])
-    nearly_outdated_event = FactoryBot.create(:event, id: 2, frequency: 4, dates: [Date.local_today + 1.week])
+    outdated_event = FactoryBot.create(:event, title: 'Corner', id: 1, frequency: 4, dates: [Date.local_today - 4.weeks])
+    nearly_outdated_event = FactoryBot.create(:event, title: 'Jam', id: 2, frequency: 4, dates: [Date.local_today + 1.week])
 
     skip_login
     visit 'outdated'
 
     expect(page).to have_content('1 event out of date, 1 event nearly out of date')
 
-    expect(page).to have_link "/events/#{outdated_event.to_param}/edit"
-    expect(page).to have_link "/events/#{nearly_outdated_event.to_param}/edit"
+    expect(page).to have_link 'Corner', href: "/events/#{outdated_event.to_param}/edit"
+    expect(page).to have_link 'Jam', href: "/events/#{nearly_outdated_event.to_param}/edit"
 
     expect(page).to have_link 'link', href: outdated_event.url
     expect(page).to have_link 'link', href: nearly_outdated_event.url
