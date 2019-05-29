239d3bb0f9454f6d6bfa38a8506a48893eb381e6
Duncan Stuart <dgmstuart@gmail.com>
2019-05-30 01:17:23 +0200

    (fix) remove space before .to in spec

--- spec/lib/dates_string_parser_spec.rb
+++ spec/lib/dates_string_parser_spec.rb
@@ -13,27 +13,27 @@ RSpec.describe DatesStringParser do
         '',
         nil
       ].each do |string|
-        expect(parse(string)). to eq []
+        expect(parse(string)).to eq []
       end
     end
 
     it 'returns a correct date when given a string with one date' do
-      expect(parse('1/2/2015')). to eq [Date.new(2015, 2, 1)]
+      expect(parse('1/2/2015')).to eq [Date.new(2015, 2, 1)]
     end
 
     it 'returns an array of correct dates when given a comma-separated string with multiple dates' do
-      expect(parse('01/12/2015, 11/07/2015,05/12/2015')). to eq [
+      expect(parse('01/12/2015, 11/07/2015,05/12/2015')).to eq [
         Date.new(2015, 12, 1), Date.new(2015, 7, 11), Date.new(2015, 12, 0o5)
       ]
     end
 
     it 'BUG: Ignores non-comma separated dates' do
-      expect(parse('5/2/2014 06/10/2012')). to eq [Date.new(2014, 2, 5)]
+      expect(parse('5/2/2014 06/10/2012')).to eq [Date.new(2014, 2, 5)]
     end
 
     it 'BUG: Accepts duplicated dates' do
-      expect(parse('5/6/2014, 05/06/2014')). to eq [Date.new(2014, 6, 5), Date.new(2014, 6, 5)]
-      expect(parse('05/06/2014, 05/06/2014')). to eq [Date.new(2014, 6, 5), Date.new(2014, 6, 5)]
+      expect(parse('5/6/2014, 05/06/2014')).to eq [Date.new(2014, 6, 5), Date.new(2014, 6, 5)]
+      expect(parse('05/06/2014, 05/06/2014')).to eq [Date.new(2014, 6, 5), Date.new(2014, 6, 5)]
     end
 
     it 'returns an empty string when the string is not parseable as a date' do
