2936eb110b4099bb7a0f66db1f4b41beecce1a12
Duncan Stuart <dgmstuart@gmail.com>
2019-06-06 01:26:15 +0200

    (feature) Show associated events on venues
    Since it's not possible to delete venues with associated events, we
    should display those events on the venues.

---
+++ app/presenters/associated_event.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+class AssociatedEvent
+  def initialize(event, summarizer: EventSummarizer.new, url_helpers: Rails.application.routes.url_helpers)
+    @event = event
+    @summarizer = summarizer
+    @url_helpers = url_helpers
+  end
+
+  def summary
+    summarizer.summarize(event)
+  end
+
+  def link
+    url_helpers.event_path(event)
+  end
+
+  private
+
+  attr_reader :event, :summarizer, :url_helpers
+end
