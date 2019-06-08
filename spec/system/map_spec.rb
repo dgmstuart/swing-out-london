606c07f893069efd396ae1c5da20f682423822fd
Duncan Stuart <dgmstuart@gmail.com>
2019-06-08 14:48:23 +0200

    (spec) Cover map info windows
    This isn't ideal since we're checking the format of the window in the
    JSON rather than actually in the page, but it gives me some confidence
    to refactor.

--- spec/system/map_spec.rb
+++ spec/system/map_spec.rb
@@ -5,6 +5,20 @@ require 'rails_helper'
 RSpec.describe 'Users can view a map of upcoming events' do
   describe 'socials page' do
     it 'viewing the page' do
+      venue = FactoryBot.create(
+        :venue,
+        name: 'The Boudoir Club',
+        address: '22 Night Street',
+        postcode: 'ZZ2 2ZZ'
+      )
+      FactoryBot.create(
+        :social,
+        venue: venue,
+        title: 'Bedroom Bounce',
+        url: 'https://bb.com',
+        dates: [Date.new(2019, 6, 8)]
+      )
+
       Timecop.freeze(Time.utc(2019, 6, 4, 12)) do
         visit '/map/socials'
       end
@@ -23,6 +37,17 @@ RSpec.describe 'Users can view a map of upcoming events' do
         .and have_content('Saturday 15th June')
         .and have_content('Sunday 16th June')
         .and have_content('Monday 17th June')
+
+      marker_json = page.find('#map')['data-markers']
+      markers = JSON.parse(marker_json)
+      expect(markers.count).to eq 1
+      info_window_string = markers.first.fetch('infoWindowContent')
+      info_window = Capybara.string(info_window_string)
+      expect(info_window).to have_content('The Boudoir Club')
+      expect(info_window).to have_content('22 Night Street')
+      expect(info_window).to have_content('ZZ2 2ZZ')
+      expect(info_window).to have_link('Saturday 8th June', href: '/map/socials/2019-06-08')
+      expect(info_window).to have_link('Bedroom Bounce', href: 'https://bb.com')
     end
 
     it 'looking at a date in the past' do
@@ -36,6 +61,22 @@ RSpec.describe 'Users can view a map of upcoming events' do
 
   describe 'classes page' do
     it 'viewing the page' do
+      venue = FactoryBot.create(
+        :venue,
+        name: 'The Daylight Centre',
+        address: '9 Mornington Crescent',
+        postcode: 'DA7 1GH'
+      )
+      organiser = FactoryBot.create(:organiser, name: 'Morning Swing')
+      FactoryBot.create(
+        :class,
+        venue: venue,
+        class_organiser: organiser,
+        class_style: 'Balboa',
+        url: 'https://dlc.com',
+        day: 'Wednesday'
+      )
+
       visit '/map/classes'
 
       expect(page).to have_content('Monday')
@@ -45,6 +86,17 @@ RSpec.describe 'Users can view a map of upcoming events' do
         .and have_content('Friday')
         .and have_content('Saturday')
         .and have_content('Sunday')
+
+      marker_json = page.find('#map')['data-markers']
+      markers = JSON.parse(marker_json)
+      expect(markers.count).to eq 1
+      info_window_string = markers.first.fetch('infoWindowContent')
+      info_window = Capybara.string(info_window_string)
+      expect(info_window).to have_content('The Daylight Centre')
+      expect(info_window).to have_content('9 Mornington Crescent')
+      expect(info_window).to have_content('DA7 1GH')
+      expect(info_window).to have_content('Wednesdays')
+      expect(info_window).to have_link('Class (Balboa) with Morning Swing', href: 'https://dlc.com')
     end
 
     it 'looking at a misspelled day' do
