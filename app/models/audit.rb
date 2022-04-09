c9649796d25b98ba5160f67cdc65c36010ab9f4a
Duncan Stuart <dgmstuart@gmail.com>
2022-04-09 21:30:08 +0200

    Audit log: put edited_by first
    This is probably what you want to see first

--- app/models/audit.rb
+++ app/models/audit.rb
@@ -3,8 +3,8 @@
 class Audit < ApplicationRecord
   def as_json
     {
-      created_at: created_at,
       edited_by: editor.name,
+      created_at: created_at,
       action: action,
       record: "#{auditable_type}(#{auditable_id})",
       changes: audited_changes,
