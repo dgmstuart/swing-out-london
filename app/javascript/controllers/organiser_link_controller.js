2b00f2b0fe1ede688af38b244510cf35cefe8caf
Duncan Stuart <dgmstuart@gmail.com>
2023-03-22 02:36:13 +0100

    Add an animation when copying link
    Stolen from my own bingo card project.

--- app/javascript/controllers/organiser_link_controller.js
+++ app/javascript/controllers/organiser_link_controller.js
@@ -5,7 +5,33 @@ export default class extends Controller {
   static targets = [ "source" ]
 
   copy(event) {
-    event.preventDefault()
-    navigator.clipboard.writeText(this.sourceTarget.textContent.trim())
+    event.preventDefault();
+
+    const buttonActive = function (element, changeText) {
+      const {
+        innerText,
+        style: { color, backgroundColor },
+      } = element;
+
+      setTimeout(() => {
+        element.style.color = color;
+        element.style.backgroundColor = backgroundColor;
+        element.innerText = innerText;
+      }, 1500);
+
+      element.style.color = "#384f6e";
+      element.style.backgroundColor = "white";
+      element.innerText = changeText;
+    };
+
+    const target = event.target;
+
+    target.disabled = true;
+    buttonActive(target, "Copied");
+
+    navigator.clipboard.writeText(this.sourceTarget.textContent.trim());
+
+    target.disabled = false;
   }
+
 }
