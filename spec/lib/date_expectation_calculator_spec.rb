c128c89aefbcaa132feec57d839061995dfa5e5a
Duncan Stuart <dgmstuart@gmail.com>
2023-03-25 00:42:49 +0100

    Fix specs: couldn't find application classes
    Not sure why these started breaking - it was after a big renaming so
    perhaps they end up just being called in a different order? Either way I
    guess we're expecting them to fail, since lib classes aren't required by
    default.

--- spec/lib/date_expectation_calculator_spec.rb
+++ spec/lib/date_expectation_calculator_spec.rb
@@ -1,24 +1,26 @@
 # frozen_string_literal: true
 
-require 'rails_helper'
+require 'spec_helper'
+require 'lib/date_expectation_calculator'
+require 'active_support/core_ext/integer/time'
 
 RSpec.describe DateExpectationCalculator do
   describe '#expecting_a_date?' do
     context 'when the subject is infrequent' do
       context 'and there is no expected date' do
-        let(:calculator) { described_class.new(true, nil, Time.zone.today) }
+        let(:calculator) { described_class.new(true, nil, Time.now.utc) }
 
         specify { expect(calculator.expecting_a_date?).to be false }
       end
 
       context 'and the expected date is less than 3 months after the comparison date' do
-        let(:calculator) { described_class.new(true, Time.zone.today + 1.month, Time.zone.today) }
+        let(:calculator) { described_class.new(true, Time.now.utc + 1.month, Time.now.utc) }
 
         specify { expect(calculator.expecting_a_date?).to be true }
       end
 
       context 'and the expected date is more than 3 months after the comparison date' do
-        let(:calculator) { described_class.new(true, Time.zone.today + 4.months, Time.zone.today) }
+        let(:calculator) { described_class.new(true, Time.now.utc + 4.months, Time.now.utc) }
 
         specify { expect(calculator.expecting_a_date?).to be false }
       end
@@ -26,19 +28,19 @@ RSpec.describe DateExpectationCalculator do
 
     context 'when the subject is frequent' do
       context 'and there is no expected date' do
-        let(:calculator) { described_class.new(false, nil, Time.zone.today) }
+        let(:calculator) { described_class.new(false, nil, Time.now.utc) }
 
         specify { expect(calculator.expecting_a_date?).to be true }
       end
 
       context 'and the expected date is less than 3 months after the comparison date' do
-        let(:calculator) { described_class.new(false, Time.zone.today + 1.month, Time.zone.today) }
+        let(:calculator) { described_class.new(false, Time.now.utc + 1.month, Time.now.utc) }
 
         specify { expect(calculator.expecting_a_date?).to be true }
       end
 
       context 'and the expected date is more than 3 months after the comparison date' do
-        let(:calculator) { described_class.new(false, Time.zone.today + 4.months, Time.zone.today) }
+        let(:calculator) { described_class.new(false, Time.now.utc + 4.months, Time.now.utc) }
 
         specify { expect(calculator.expecting_a_date?).to be true }
       end
--- spec/lib/dates_string_parser_spec.rb
+++ spec/lib/dates_string_parser_spec.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
-require 'rails_helper'
+require 'spec_helper'
+require 'lib/dates_string_parser'
+require 'active_support/core_ext/string/conversions'
 
 RSpec.describe DatesStringParser do
   describe '#parse' do
