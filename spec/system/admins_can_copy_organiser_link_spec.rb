d60902908749174b1e7435748f88989ae741bfdb
Duncan Stuart <dgmstuart@gmail.com>
2023-03-31 18:52:29 +0200

    Backfill spec: regenerate organiser link
    This feels like a slightly hacky way to test the updated link, but the
    alternative would be to stub the token generator and I'd rather not.
    
    Note that we need the `expect(page).not_to have_content(url)` so that
    Capybara will wait for the event to be updated before we try reloading
    it.

--- spec/system/admins_can_copy_organiser_link_spec.rb
+++ spec/system/admins_can_copy_organiser_link_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe "Admins can copy an organiser link" do
   include System::ClipboardHelper
 
   context "when an organiser token exists", :js do
-    it "shows a url which will allow an organiser to edit an event" do
+    it "shows a url which will allow an organiser to edit an event, and allows the admin to change the link" do
       event = create(:event, organiser_token: "abc123")
       grant_clipboard_permissions
 
@@ -20,6 +20,17 @@ RSpec.describe "Admins can copy an organiser link" do
       click_on "Copy"
 
       expect(clipboard_text).to eq(url)
+
+      click_on "Generate link"
+
+      page.driver.browser.switch_to.alert.accept
+
+      expect(page).not_to have_content(url)
+
+      event.reload
+      expect(event.organiser_token).not_to eq "abc123"
+      new_url = URI.join(page.server_url, "/external_events/#{event.organiser_token}/edit").to_s
+      expect(page).to have_content(new_url)
     end
   end
 
