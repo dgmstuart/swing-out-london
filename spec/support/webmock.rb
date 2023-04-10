695d694d0173ca951dad9539bd5b8433cefc4f95
Duncan Stuart <dgmstuart@gmail.com>
2023-04-10 02:19:57 +0200

    Attempt to fix "too many open files" error
    I don't really understand this, but it's worth a try?
    
      https://github.com/bblimke/webmock/blob/master/README.md#connecting-on-nethttpstart

--- spec/support/webmock.rb
+++ spec/support/webmock.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 require "webmock/rspec"
+
 WebMock.disable_net_connect!(
   allow_localhost: true,
-  allow: "chromedriver.storage.googleapis.com"
+  allow: "chromedriver.storage.googleapis.com",
+  net_http_connect_on_start: true
 )
