4d51ef1a2017d28c3ab0440a8ef4a7200b9b52c5
Duncan Stuart <dgmstuart@gmail.com>
2023-04-27 01:47:31 +0200

    Update environment files
    Done by looking at the diffs with rails app:update - these should have
    been removed when I removed the mailers.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,12 +54,6 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "swingoutlondon_production"
 
-  # config.action_mailer.perform_caching = false
-
-  # Ignore bad email addresses and do not raise email delivery errors.
-  # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
-
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
@@ -109,7 +103,4 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-
-  # Mailer config
-  # config.action_mailer.delivery_method = :smtp
 end
