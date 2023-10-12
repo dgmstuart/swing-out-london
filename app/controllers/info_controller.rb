6ad99f0b90a757f8b4f8d0750822d822b3eb57f6
Duncan Stuart <dgmstuart@gmail.com>
2023-10-12 19:12:15 +0200

    Refactor: extract info page config to YAML

--- app/controllers/info_controller.rb
+++ app/controllers/info_controller.rb
@@ -4,11 +4,11 @@ class InfoController < ApplicationController
   before_action :set_cache_control_on_static_pages, only: %i[about listings_policy privacy]
 
   def about
-    @page = AboutPage
+    @page = InfoPage.new(:about)
   end
 
   def listings_policy
-    @page = ListingsPolicyPage
+    @page = InfoPage.new(:listings_policy)
   end
 
   def privacy; end
@@ -19,63 +19,4 @@ class InfoController < ApplicationController
     # Varnish will cache the page for 43200 seconds = 12 hours:
     response.headers["Cache-Control"] = "public, max-age=43200"
   end
-
-  class Page
-    def initialize(root:, content_partials:, sidebar_partials:)
-      @root = root
-      @content_partials = content_partials
-      @sidebar_partials = sidebar_partials
-    end
-
-    def content_partials
-      @content_partials.map { Partial.new(@root, _1) }
-    end
-
-    def sidebar_partials
-      @sidebar_partials.map { Partial.new(@root, _1) }
-    end
-
-    class Partial
-      def initialize(root, path)
-        @root = root
-        @path = path
-      end
-
-      def path
-        File.join(@root, @path)
-      end
-
-      def name
-        File.basename(path)
-      end
-    end
-  end
-
-  AboutPage = Page.new(
-    root: "info/about",
-    content_partials: %w[
-      about_swingoutlondon
-    ],
-    sidebar_partials: %w[
-      about_us
-      about_lindy
-      show_love
-    ]
-  )
-
-  ListingsPolicyPage = Page.new(
-    root: "info/listings_policy",
-    content_partials: %w[
-      intro
-      location_text
-      classes_text
-      classes_policy
-      socials_text
-      socials_policy
-      music_text
-    ],
-    sidebar_partials: %w[
-      swing_music
-    ]
-  )
 end
