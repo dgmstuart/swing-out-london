75678cce5c967748e47dfce981898e37a11fc649
Duncan Stuart <dgmstuart@gmail.com>
2023-10-14 17:11:05 +0200

    Cover InfoPage with specs
    "config" as a name is maybe a bit too general, since this only deals
    with YAML files, but I don't want to have the file type in the name
    either.

--- app/models/info_page.rb
+++ app/models/info_page.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 class InfoPage
-  def initialize(page)
-    pages_config = load_config_file("info_pages.yml")
-    @page = pages_config.fetch(page)
+  def initialize(page_name, pages: InfoPages.new, city: CITY.key)
+    @page = pages.page(page_name)
+    @city = city
   end
 
   def content_partials
@@ -17,16 +17,21 @@ class InfoPage
   private
 
   def partials(key)
-    @page.fetch(CITY.key).fetch(key).map { Partial.new(root, _1) }
+    @page.fetch(@city).fetch(key).map { Partial.new(root, _1) }
   end
 
   def root
     @page.fetch(:dir_path)
   end
 
-  def load_config_file(filename)
-    file_path = Rails.root.join("config", filename)
-    YAML.load_file(file_path).deep_symbolize_keys
+  class InfoPages
+    def initialize(config: Config.new.load("info_pages.yml"))
+      @config = config
+    end
+
+    def page(page_name)
+      @config.fetch(page_name)
+    end
   end
 
   class Partial
---
+++ spec/models/info_page_spec.rb
@@ -0,0 +1,56 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+require "app/models/info_page"
+
+describe InfoPage do
+  describe "#content_partials" do
+    it "returns the list of content partials for the current city" do # rubocop:disable RSpec/ExampleLength
+      config = {
+        dir_path: "foo/about",
+        london: {
+          content: %w[about_london london_history]
+        },
+        bristol: {
+          content: %w[about_bristol bristol_history]
+        }
+      }
+      pages = instance_double("InfoPages", page: config)
+
+      page = described_class.new(:about, pages:, city: :london)
+
+      aggregate_failures do
+        partials = page.content_partials
+        expect(partials[0].path).to eq "foo/about/about_london"
+        expect(partials[0].name).to eq "about_london"
+        expect(partials[1].path).to eq "foo/about/london_history"
+        expect(partials[1].name).to eq "london_history"
+      end
+    end
+  end
+
+  describe "#sidebar_partials" do
+    it "returns the list of content partials for the current city" do # rubocop:disable RSpec/ExampleLength
+      config = {
+        dir_path: "foo/about",
+        london: {
+          sidebar: %w[lindy_hop about_us_london]
+        },
+        bristol: {
+          sidebar: %w[lindy_hop about_us_bristol]
+        }
+      }
+      pages = instance_double("InfoPages", page: config)
+
+      page = described_class.new(:about, pages:, city: :bristol)
+
+      aggregate_failures do
+        partials = page.sidebar_partials
+        expect(partials[0].path).to eq "foo/about/lindy_hop"
+        expect(partials[0].name).to eq "lindy_hop"
+        expect(partials[1].path).to eq "foo/about/about_us_bristol"
+        expect(partials[1].name).to eq "about_us_bristol"
+      end
+    end
+  end
+end
