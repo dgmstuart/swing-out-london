913cab72828d6d5cb6b14d703bdfad321b402142
Duncan Stuart <dgmstuart@gmail.com>
2023-10-18 00:03:05 +0200

    Log in with Slack
    Slack supports OpenID connect:
    
      https://api.slack.com/authentication/sign-in-with-slack
    
    A few things to note:
    
    - The Omniauth approach uses OpenID connect - it seems like the approach
      is maybe a little different to Facebook, but the output is largely the
      same.
    - We don't get callback URLs for free for some reason. In order to
      generate the full url ("redirect_uri") we need to somehow get the
      host. Since we're not in a request-response cycle when we configure
      OmniAuth, the only approach I know of is to get it from an env
      variable.
    - We don't need the allow list of admins anymore: if you're in Slack,
      that's good enough. Instead let's allow you to optionally skip login
      (or use real login if you're eg. using ngrok)
    - The Slack "team" is the swingoutlondon slack.
    
    Button styling and SVG is copied from Slack's button generator:
    
      https://api.slack.com/authentication/sign-in-with-slack
    
    More code-level things:
    
    We don't actually need to be able to stub ID anymore, but it's more work
    to remove it than keep it.

--- lib/omniauth_test_response_builder.rb
+++ lib/omniauth_test_response_builder.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require "faker"
+require "faker/slack"
 
 class OmniauthTestResponseBuilder
   def initialize(hash_builder: OmniAuth::AuthHash, mock_auth_config: OmniAuth.config.mock_auth)
@@ -9,37 +10,80 @@ class OmniauthTestResponseBuilder
   end
 
   def stub_auth_hash(
-    id: Faker::Facebook.uid,
+    id: Faker::Slack.uid,
     name: Faker::Name.lindy_hop_name,
     token: SecureRandom.hex
   )
     raise "Can't stub authentication in production" if Rails.env.production?
 
-    auth_hash = facebook_auth_hash(id:, name:, token:)
-    mock_auth_config[:facebook] = auth_hash
+    auth_hash = slack_auth_hash(id:, name:, token:)
+    mock_auth_config[:slack] = auth_hash
   end
 
   private
 
   attr_reader :hash_builder, :mock_auth_config
 
-  def facebook_auth_hash(id:, name:, token:)
+  def slack_auth_hash(id:, name:, token:)
+    first_name, *other_names = name.split
+    last_name = other_names.join(" ")
     hash_builder.new(
-      "provider" => "facebook",
+      "provider" => :slack,
       "uid" => id,
       "info" => {
         "name" => name,
-        "image" => "http://graph.facebook.com/v2.10/#{id}/picture"
+        "email" => nil,
+        "email_verified" => nil,
+        "nickname" => nil,
+        "first_name" => first_name,
+        "last_name" => last_name,
+        "gender" => nil,
+        "image" => "https://example.com/avatar.jpg",
+        "phone" => nil,
+        "urls" => { "website" => nil }
       },
       "credentials" => {
+        "id_token" => "a-very-long-token",
         "token" => token,
-        "expires_at" => 1546086985,
-        "expires" => true
+        "refresh_token" => nil,
+        "expires_in" => nil,
+        "scope" => nil
       },
       "extra" => {
         "raw_info" => {
+          "ok" => true,
+          "sub" => id,
+          "https://slack.com/user_id" => id,
+          "https://slack.com/team_id" => "TEKLWQYP5",
           "name" => name,
-          "id" => id
+          "picture" => "https://avatars.slack-edge.com/2022-02-02/3067504073456_edeaee73989a69e2787a_512.jpg",
+          "given_name" => first_name,
+          "family_name" => last_name,
+          "locale" => "en-US",
+          "https://slack.com/team_name" => "Swing Out London",
+          "https://slack.com/team_domain" => "swingoutlondon",
+          "https://slack.com/user_image_24" => "https://avatars.slack-edge.com/2022-02-02/3067504073456_edeaee73989a69e2787a_24.jpg",
+          "https://slack.com/user_image_32" => "https://avatars.slack-edge.com/2022-02-02/3067504073456_edeaee73989a69e2787a_32.jpg",
+          "https://slack.com/user_image_48" => "https://avatars.slack-edge.com/2022-02-02/3067504073456_edeaee73989a69e2787a_48.jpg",
+          "https://slack.com/user_image_72" => "https://avatars.slack-edge.com/2022-02-02/3067504073456_edeaee73989a69e2787a_72.jpg",
+          "https://slack.com/user_image_192" => "https://avatars.slack-edge.com/2022-02-02/3067504073456_edeaee73989a69e2787a_192.jpg",
+          "https://slack.com/user_image_512" => "https://avatars.slack-edge.com/2022-02-02/3067504073456_edeaee73989a69e2787a_512.jpg",
+          "https://slack.com/user_image_1024" => "https://avatars.slack-edge.com/2022-02-02/3067504073456_edeaee73989a69e2787a_1024.jpg",
+          "https://slack.com/team_image_34" => "https://avatars.slack-edge.com/2018-12-05/495828053858_54cf95194971a730f2c5_34.jpg",
+          "https://slack.com/team_image_44" => "https://avatars.slack-edge.com/2018-12-05/495828053858_54cf95194971a730f2c5_44.jpg",
+          "https://slack.com/team_image_68" => "https://avatars.slack-edge.com/2018-12-05/495828053858_54cf95194971a730f2c5_68.jpg",
+          "https://slack.com/team_image_88" => "https://avatars.slack-edge.com/2018-12-05/495828053858_54cf95194971a730f2c5_88.jpg",
+          "https://slack.com/team_image_102" => "https://avatars.slack-edge.com/2018-12-05/495828053858_54cf95194971a730f2c5_102.jpg",
+          "https://slack.com/team_image_132" => "https://avatars.slack-edge.com/2018-12-05/495828053858_54cf95194971a730f2c5_132.jpg",
+          "https://slack.com/team_image_230" => "https://avatars.slack-edge.com/2018-12-05/495828053858_54cf95194971a730f2c5_230.jpg",
+          "https://slack.com/team_image_default" => false,
+          "iss" => "https://slack.com",
+          "aud" => "495710848787.6049331372885",
+          "exp" => 1697583583,
+          "iat" => 1697583283,
+          "auth_time" => 1697583283,
+          "nonce" => "80d86a2cb8a43ee94d100618066aa1bc",
+          "at_hash" => "45120de6679f0cad2b2869"
         }
       }
     )
--- spec/support/system/auth_helper.rb
+++ spec/support/system/auth_helper.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 require "support/system/omniauth_helper"
-require "faker/facebook"
 
 module System
   module AuthHelper
@@ -13,15 +12,14 @@ module System
       end
     end
 
-    def stub_login(id: Faker::Facebook.uid, name: Faker::Name.lindy_hop_name)
+    def stub_login(id: Faker::Slack.uid, name: Faker::Name.lindy_hop_name)
       stub_auth_hash(id:, name:)
-      Rails.application.config.x.facebook.admin_user_ids = [id]
     end
 
     def skip_login
       user = instance_double(LoginSession::User,
                              name: Faker::Name.lindy_hop_name,
-                             auth_id: Faker::Facebook.uid,
+                             auth_id: Faker::Slack.uid,
                              logged_in?: true)
       login_session = instance_double(LoginSession, "Fake login", user:)
       allow(LoginSession).to receive(:new).and_return(login_session)
