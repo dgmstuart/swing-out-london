fa856e910bba99d5a86bff15164cb2aa1dd35b80
Duncan Stuart <dgmstuart@gmail.com>
2023-10-28 21:56:37 +0200

    Refactor: Extract FacebookGraphApi::UserApi
    This is kind of an adapter I guess? A way to create a nicer interface.
    
    Note that since the http client needs a user to create it, the pattern
    of creating a generic http client and passing it in to the main API
    class doesn't actually make sense.
    
    This feels just like glue code - it's entirely exercised by the rest of
    the test suite, and I don't feel like it has any properties we care
    about testing.

---
+++ lib/facebook_graph_api/user_api.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+require "facebook_graph_api/http_client"
+require "facebook_graph_api/api"
+
+module FacebookGraphApi
+  class UserApi
+    def initialize(
+      user,
+      http_client_builder: FacebookGraphApi::HttpClient,
+      api_builder: FacebookGraphApi::Api
+    )
+      @user = user
+      @http_client_builder = http_client_builder
+      @api_builder = api_builder
+    end
+
+    def revoke_login
+      api.revoke_login(user.auth_id)
+    end
+
+    private
+
+    attr_reader :user, :api_builder, :http_client_builder
+
+    def api
+      client = http_client_builder.new(auth_token: user.token)
+      api_builder.new(client)
+    end
+  end
+end
