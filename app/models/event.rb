e700168ec0e70e8ac22ccf8bd8917d447f5e8720
Duncan Stuart <dgmstuart@gmail.com>
2023-10-29 16:09:53 +0100

    Fix: Archiving events with no dates
    This wasn't working because dates can be an empty array or empty string?

--- app/models/event.rb
+++ app/models/event.rb
@@ -288,12 +288,11 @@ class Event < ApplicationRecord
 
   def archive!
     return false if !last_date.nil? && last_date < Date.current
-
     # If there's already a last_date in the past, then the event should already be archived!
 
     self[:last_date] = if weekly?
                          Date.current.prev_occurring(day.downcase.to_sym)
-                       elsif dates.nil?
+                       elsif dates.blank?
                          Date.new # Earliest possible ruby date
                        else
                          latest_date
