39f7b34b520c5482a0a630bd3ede13bd61ba29bf
Duncan Stuart <dgmstuart@gmail.com>
2023-11-04 14:34:47 +0100

    Fix: Form shared example accepts strings
    Since the attribute type is String these got converted to eg
    
        "\[\"12/20/2010\"\]"
    
    This worked before because we had no validation on the actual value of
    the dates field and these specs were just checking whether the value was
    empty.

--- spec/support/shared_examples/events/form/validates_event_with_dates.rb
+++ spec/support/shared_examples/events/form/validates_event_with_dates.rb
@@ -2,12 +2,12 @@
 
 RSpec.shared_examples "validates event with dates (form)" do |model_name|
   it "is invalid if it's weekly and has dates" do
-    model = build(model_name, frequency: 1, day: 5, dates: ["12/10/2010"])
+    model = build(model_name, frequency: 1, day: 5, dates: "12/10/2010")
     model.valid?
     expect(model.errors.messages).to eq(dates: ["must be empty for weekly events"])
   end
 
   it "is valid if it's occasional and has dates" do
-    expect(build(model_name, frequency: 0, dates: ["12/10/2010"])).to be_valid
+    expect(build(model_name, frequency: 0, dates: "12/10/2010")).to be_valid
   end
 end
