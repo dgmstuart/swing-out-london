91b3bbe838c53a43ae46539d3310c6bc2c0f98a9
Duncan Stuart <dgmstuart@gmail.com>
2023-11-04 01:51:38 +0100

    Generate CSS source maps in development
    Use --embed-sources
    
      https://sass-lang.com/documentation/cli/dart-sass/#embed-sources
    
    ...because dartsass generates links to eg.
    
        /stylesheets/shared/base.scss
    
    ...whereas in reality in development the source files are available
    under /assets:
    
        /assets/shared/base.scss
    
    Maybe there's a way to configure or remap those, but I couldn't find
    any.

--- config/initializers/dartsass.rb
+++ config/initializers/dartsass.rb
@@ -1,6 +1,14 @@
 # frozen_string_literal: true
 
-Rails.application.config.dartsass.builds = {
-  "application.scss" => "application.css",
-  "application_cms.scss" => "application_cms.css"
-}
+Rails.application.configure do
+  config.dartsass.builds = {
+    "application.scss" => "application.css",
+    "application_cms.scss" => "application_cms.css"
+  }
+  config.dartsass.build_options =
+    if Rails.env.development?
+      "--style=compressed --embed-sources"
+    else
+      "--style=compressed --no-source-map"
+    end
+end
