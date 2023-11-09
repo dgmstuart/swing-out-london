5764cf99f905c5022ab45372059e1495c3be2d6d
Duncan Stuart <dgmstuart@gmail.com>
2023-11-09 14:20:09 +0100

    Refactor: DatesStringValidator delegates to form
    Create a form object for validating individual dates.
    
    Note that if we actually try to show `.messages` for these dates then it
    will fail since when a symbol is passed, Rails expects it to be a
    translation key. But at least at the moment we're never doing that so
    maybe it's YAGNI to try.
    
    I guess maybe the desscriptions for the aggregated dates errors would
    also live in I18n, but for now it feels simpler in a hash, since that
    also gives us a hard exit in case there's an unexpected validation
    error, rather than just returning a "translation not found" error.
    
    There's coupling between DatesStringValidator and DateForm: they need to
    have exactly the same list of error types. That's fine while DateForm is
    a nested class?
    
    Note that we need to move the require of the shared examples before the
    require for the validator, so that we can avoid having to include all
    the validators individually.

--- app/validators/dates_string_validator.rb
+++ app/validators/dates_string_validator.rb
@@ -3,52 +3,60 @@
 require "date_string_parser"
 
 class DatesStringValidator < ActiveModel::EachValidator
-  SOLDN_START_DATE = Date.parse("2010-08-16")
-
   def validate_each(record, attribute, value)
     return if value.blank?
 
     date_strings = value.split(",").map(&:strip)
     problems = problem_date_strings(date_strings)
 
-    errors = ErrorAdder.new(record, attribute)
-    errors.add("contained some invalid dates", problems[:invalid]) if problems[:invalid].any?
-    errors.add("contained some dates in the past", problems[:past]) if problems[:past].any?
-    errors.add("contained some dates unreasonably far in the future", problems[:too_far_future]) if problems[:too_far_future].any?
+    problems.each do |type, problem_dates|
+      description = {
+        invalid: "contained some invalid dates",
+        past: "contained some dates in the past",
+        distant_past: "contained some dates in the distant past",
+        distant_future: "contained some dates unreasonably far in the future"
+      }.fetch(type)
+      message = "#{description}: #{problem_dates.join(', ')}"
+
+      record.errors.add(attribute, message)
+    end
   end
 
   private
 
-  class ErrorAdder
-    def initialize(record, attribute)
-      @record = record
-      @attribute = attribute
-    end
-
-    def add(description, problem_dates)
-      message = "#{description}: #{problem_dates.join(', ')}"
-      @record.errors.add(@attribute, message)
-    end
-  end
-
   def problem_date_strings(date_strings)
     parser = DateStringParser.new
-    date_strings.each_with_object({ invalid: [], past: [], too_far_future: [] }) do |date_string, problems|
+    date_strings.each_with_object(new_hash_of_arrays) do |date_string, problems|
       date = parser.parse(date_string)
       if date
-        problems[:past] << date_string if date < past_cutoff
-        problems[:too_far_future] << date_string if date > 2.years.from_now
+        form = DateForm.new(date, allow_past: options[:allow_past])
+        form.errors.each { |error| problems[error.type] << date_string } if form.invalid?
       else
         problems[:invalid] << %("#{date_string}")
       end
     end
   end
 
-  def past_cutoff
-    if options[:allow_past]
-      SOLDN_START_DATE
-    else
-      Date.current
+  def new_hash_of_arrays
+    Hash.new { |hash, key| hash[key] = [] }
+  end
+
+  class DateForm
+    include ActiveModel::Validations
+
+    def initialize(date, allow_past:)
+      @date = date
+      @allow_past = allow_past
     end
+
+    attr_reader :date
+
+    validates :date, past_date: true, if: -> { !allow_past }
+    validates :date, distant_past_date: true, if: -> { allow_past }
+    validates :date, distant_future_date: true
+
+    private
+
+    attr_reader :allow_past
   end
 end
