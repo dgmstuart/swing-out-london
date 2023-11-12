7258a1718306d32bd8d91bd283a52fecb81b6355
Duncan Stuart <dgmstuart@gmail.com>
2023-11-12 23:35:09 +0100

    Rebuild schema
    Not sure if these changes are from Heroku's database or from Rails
    updates. Either way, let's establish a baseline.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[6.1].define(version: 2023_03_28_083046) do
+ActiveRecord::Schema[7.1].define(version: 2023_03_28_083046) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,7 +29,7 @@ ActiveRecord::Schema[6.1].define(version: 2023_03_28_083046) do
     t.text "comment"
     t.string "remote_address"
     t.string "request_uuid"
-    t.datetime "created_at"
+    t.datetime "created_at", precision: nil
     t.index ["associated_type", "associated_id"], name: "associated_index"
     t.index ["auditable_type", "auditable_id", "version"], name: "auditable_index"
     t.index ["created_at"], name: "index_audits_on_created_at"
@@ -40,14 +40,11 @@ ActiveRecord::Schema[6.1].define(version: 2023_03_28_083046) do
   create_table "events", id: :serial, force: :cascade do |t|
     t.string "title", limit: 255
     t.string "day", limit: 255
-    t.string "event_type", limit: 255
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", precision: nil
+    t.datetime "updated_at", precision: nil
     t.integer "venue_id", null: false
     t.integer "frequency"
     t.string "url", limit: 255, null: false
-    t.string "date_array", limit: 255
-    t.string "cancellation_array", limit: 255
     t.date "first_date"
     t.date "last_date"
     t.text "class_style"
@@ -57,13 +54,14 @@ ActiveRecord::Schema[6.1].define(version: 2023_03_28_083046) do
     t.boolean "has_social"
     t.integer "class_organiser_id"
     t.integer "social_organiser_id"
-    t.date "expected_date"
     t.string "organiser_token"
-    t.index ["event_type"], name: "index_events_on_event_type"
+    t.string "event_type"
+    t.string "date_array"
+    t.string "cancellation_array"
+    t.date "expected_date"
     t.index ["frequency", "day", "has_class"], name: "index_events_on_fq_and_day_and_has_class"
     t.index ["frequency", "day", "has_social"], name: "index_events_on_fq_and_day_and_has_social"
     t.index ["frequency", "has_class"], name: "index_events_on_fq_and_has_class"
-    t.index ["last_date", "event_type"], name: "index_events_on_last_date_and_event_type"
     t.index ["last_date", "frequency", "has_class"], name: "index_events_on_last_date_and_fq_and_has_class"
     t.index ["organiser_token"], name: "index_events_on_organiser_token", unique: true
     t.index ["venue_id"], name: "index_events_on_venue_id"
@@ -85,15 +83,15 @@ ActiveRecord::Schema[6.1].define(version: 2023_03_28_083046) do
     t.string "name", limit: 255, null: false
     t.string "website", limit: 255
     t.text "description"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", precision: nil
+    t.datetime "updated_at", precision: nil
     t.string "shortname", limit: 255
     t.index ["shortname"], name: "index_organisers_on_shortname", unique: true
   end
 
   create_table "swing_dates", id: :serial, force: :cascade do |t|
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", precision: nil
+    t.datetime "updated_at", precision: nil
     t.date "date"
     t.index ["date"], name: "index_swing_dates_on_date", unique: true
   end
@@ -103,12 +101,12 @@ ActiveRecord::Schema[6.1].define(version: 2023_03_28_083046) do
     t.text "address"
     t.string "postcode", limit: 255
     t.string "website", limit: 255
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", precision: nil
+    t.datetime "updated_at", precision: nil
     t.string "area", limit: 255
-    t.string "compass", limit: 255
     t.decimal "lat", precision: 15, scale: 10
     t.decimal "lng", precision: 15, scale: 10
+    t.string "compass"
   end
 
 end
