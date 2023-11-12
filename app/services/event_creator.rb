4e506f8bfd8609e89ea12ef5fa8a490bd6e6d700
Duncan Stuart <dgmstuart@gmail.com>
2023-11-12 21:59:15 +0100

    Extract common date augmentation code
    These names are all bad.
    
    I don't think this needs its own tests: it's entirely tested through the
    two usages.

--- app/services/event_creator.rb
+++ app/services/event_creator.rb
@@ -1,31 +1,17 @@
 # frozen_string_literal: true
 
 class EventCreator
-  def initialize(repository = Event)
+  def initialize(repository = Event, attr_adaptor: EventDatesAttrs.new)
     @repository = repository
+    @attr_adaptor = attr_adaptor
   end
 
   def create!(attrs)
-    replace_key_with_swing_dates(attrs, :dates, :swing_dates)
-    replace_key_with_swing_dates(attrs, :cancellations, :swing_cancellations)
-    repository.create!(attrs)
+    attrs_with_dates = attr_adaptor.transform(attrs)
+    repository.create!(attrs_with_dates)
   end
 
   private
 
-  attr_reader :repository
-
-  def replace_key_with_swing_dates(hash, old_key, new_key)
-    replace_key(hash, old_key, new_key) { date_records(_1) }
-  end
-
-  def replace_key(hash, old_key, new_key, &)
-    old_value = hash.delete(old_key) { return hash }
-    new_value = yield(old_value)
-    hash.merge!(new_key => new_value)
-  end
-
-  def date_records(dates)
-    dates.map { |date| SwingDate.find_or_initialize_by(date:) }
-  end
+  attr_reader :repository, :attr_adaptor
 end
