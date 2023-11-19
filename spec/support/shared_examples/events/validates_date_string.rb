15ec1186aba815bf666a0331483128163065367f
Duncan Stuart <dgmstuart@gmail.com>
2023-11-19 02:29:09 +0100

    Fix: use attribute in shared example
    We're still hardcoding `:dates`, so this wasn't actually testing these
    cases for cancellations.

--- spec/support/shared_examples/events/validates_date_string.rb
+++ spec/support/shared_examples/events/validates_date_string.rb
@@ -53,47 +53,47 @@ RSpec.shared_examples "validates date string" do |attribute, model_name, options
 
   if options[:allow_past]
     it "is valid if dates are in the past" do
-      model = build(model_name, dates: "31/05/2012")
+      model = build(model_name, attribute => "31/05/2012")
       expect(model).to be_valid
     end
 
     it "is invalid if dates are earlier than the start of SOLDN" do
-      model = build(model_name, dates: "15/08/2010")
+      model = build(model_name, attribute => "15/08/2010")
       model.valid?
-      expect(model.errors.messages).to eq(dates: ["contained some dates in the distant past: 15/08/2010"])
+      expect(model.errors.messages).to eq(attribute => ["contained some dates in the distant past: 15/08/2010"])
     end
   else
     it "is invalid if dates are in the past" do
-      model = build(model_name, dates: "31/05/2012")
+      model = build(model_name, attribute => "31/05/2012")
       model.valid?
-      expect(model.errors.messages).to eq(dates: ["contained some dates in the past: 31/05/2012"])
+      expect(model.errors.messages).to eq(attribute => ["contained some dates in the past: 31/05/2012"])
     end
   end
 
   it "is invalid if dates are too far in the future" do
-    model = build(model_name, dates: "02/06/2014")
+    model = build(model_name, attribute => "02/06/2014")
     model.valid?
-    expect(model.errors.messages).to eq(dates: ["contained some dates unreasonably far in the future: 02/06/2014"])
+    expect(model.errors.messages).to eq(attribute => ["contained some dates unreasonably far in the future: 02/06/2014"])
   end
 
   it "is invalid with duplicate dates" do
-    model = build(model_name, dates: "12/07/2012,12/07/2012")
+    model = build(model_name, attribute => "12/07/2012,12/07/2012")
     model.valid?
-    expect(model.errors.messages).to eq(dates: ["contained some dates more than once: 12/07/2012"])
+    expect(model.errors.messages).to eq(attribute => ["contained some dates more than once: 12/07/2012"])
   end
 
   it "only reports duplicate future dates once as future" do
-    model = build(model_name, dates: "01/02/2020,01/02/2020")
+    model = build(model_name, attribute => "01/02/2020,01/02/2020")
     model.valid?
     expect(model.errors.messages).to eq(
-      dates: ["contained some dates unreasonably far in the future: 01/02/2020",
+      attribute => ["contained some dates unreasonably far in the future: 01/02/2020",
               "contained some dates more than once: 01/02/2020"]
     )
   end
 
   it "reports duplicated non-dates twice" do
-    model = build(model_name, dates: "30/02/2012,30/02/2012")
+    model = build(model_name, attribute => "30/02/2012,30/02/2012")
     model.valid?
-    expect(model.errors.messages).to eq(dates: ['contained some invalid dates: "30/02/2012", "30/02/2012"'])
+    expect(model.errors.messages).to eq(attribute => ['contained some invalid dates: "30/02/2012", "30/02/2012"'])
   end
 end
