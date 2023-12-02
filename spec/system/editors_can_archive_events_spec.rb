7a4c37c79b3951ae831e51c491d94f3206c2cb6f
Duncan Stuart <dgmstuart@gmail.com>
2023-12-02 19:57:14 +0100

    Backfill specs to document archive edge cases

--- spec/system/editors_can_archive_events_spec.rb
+++ spec/system/editors_can_archive_events_spec.rb
@@ -47,6 +47,42 @@ RSpec.describe "Editors can archive events" do
     expect(page).to have_content("Last date: Monday 1st January") # earliest possible Date
   end
 
+  context "when the event is already archived" do
+    it "behaves the same as success" do
+      skip_login
+      event = create(:event, frequency: 0, dates: ["02/01/2000".to_date])
+
+      visit "/events"
+
+      event.archive!
+
+      Timecop.freeze(Time.zone.local(2000, 1, 8)) do
+        click_link "Archive", match: :first
+      end
+
+      click_show
+
+      expect(page).to have_content("Last date: Sunday 2nd January")
+    end
+  end
+
+  context "when the event couldn't be saved for some reason" do
+    it "raises an error" do
+      skip_login
+      event = create(:event, frequency: 0, dates: ["02/01/2000".to_date])
+      # Set frequency to nil to simulate failed save by making the record invalid:
+      event.update_attribute(:frequency, nil) # rubocop:disable Rails/SkipsModelValidations
+
+      visit "/events"
+
+      Timecop.freeze(Time.zone.local(2000, 1, 8)) do
+        expect do
+          click_link "Archive", match: :first
+        end.to raise_error(ActiveRecord::RecordInvalid)
+      end
+    end
+  end
+
   def click_show
     within ".actions.last", match: :first do
       click_link "Show"
