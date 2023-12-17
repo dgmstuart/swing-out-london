afb848f90edd171e3e84c90b2c0fa82eb400b48b
Duncan Stuart <dgmstuart@gmail.com>
2023-12-18 00:17:00 +0100

    WIP

--- spec/form_spec_helper.rb
+++ spec/form_spec_helper.rb
@@ -1,34 +1,43 @@
 # frozen_string_literal: true
 
-require "spec_helper"
-require "active_model"
-require "factory_bot"
-require "spec/support/forms/shoulda_matchers"
+if RSpec.configuration.files_to_run.one?
+  require "spec_helper"
+  require "active_model"
+  require "factory_bot"
+  require "spec/support/forms/shoulda_matchers"
 
-FactoryBot.find_definitions if FactoryBot.factories.none?
+  FactoryBot.find_definitions if FactoryBot.factories.none?
 
-RSpec.configure do |config|
-  config.include FactoryBot::Syntax::Methods
-  config.include Shoulda::Matchers::ActiveModel
-end
+  RSpec.configure do |config|
+    config.include FactoryBot::Syntax::Methods
+  end
 
-# MONKEY PATCH
-module Shoulda
-  module Matchers
-    module RailsShim
-      class << self
-        def serialized_attributes_for(model)
-          # the real definition depends on ::ActiveRecord::Type::Serialized, which we're not loading in these isolated specs:
-          # https://github.com/thoughtbot/shoulda-matchers/issues/1553
-          attribute_types_for(model)
+  # MONKEY PATCH
+  module Shoulda
+    module Matchers
+      module RailsShim
+        class << self
+          def serialized_attributes_for(model)
+            # the real definition depends on ::ActiveRecord::Type::Serialized,
+            # which we're not loading in these isolated specs:
+            # https://github.com/thoughtbot/shoulda-matchers/issues/1553
+            attribute_types_for(model)
+          end
         end
       end
     end
   end
+
+  # Add locale to the path for Faker, unless it's already been set up and frozen by a previous spec
+  I18n.load_path << "./config/locales/en.yml"
+
+else
+  require "rails_helper"
 end
 
-# Add locale to the path for Faker
-I18n.load_path << "./config/locales/en.yml"
+RSpec.configure do |config|
+  config.include Shoulda::Matchers::ActiveModel
+end
 
 def stub_model_name(model_class_name)
   model_name = instance_double("ActiveModel::Name",
