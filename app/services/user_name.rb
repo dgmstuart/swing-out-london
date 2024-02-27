4f571c5d56e0cb3e72478b6fef2473218c92e15f
Duncan Stuart <dgmstuart@gmail.com>
2024-02-27 20:05:50 +0100

    Handle when we couldn't fetch name from Facebook
    Eg. if the ID doesn't exist we'll get this.
    
    Note that there seems to just be one response code for failures (400) -
    there isn't a separate 404.

--- app/services/user_name.rb
+++ app/services/user_name.rb
@@ -3,19 +3,24 @@
 class UserName
   def initialize(
     user:,
-    api_builder: FacebookGraphApi::UserApi
+    api_builder: FacebookGraphApi::UserApi,
+    error_reporter: Rollbar
   )
     @user = user
     @api_builder = api_builder
+    @error_reporter = error_reporter
   end
 
   def name_for(user_id)
     api = api_builder.for_user(user)
     profile = api.profile(user_id)
     profile.fetch("name")
+  rescue FacebookGraphApi::HttpClient::ResponseError => e
+    error_reporter.error(e)
+    "[Unknown user]"
   end
 
   private
 
-  attr_reader :user, :api_builder
+  attr_reader :user, :api_builder, :error_reporter
 end
--- spec/services/user_name_spec.rb
+++ spec/services/user_name_spec.rb
@@ -2,6 +2,7 @@
 
 require "spec_helper"
 require "app/services/user_name"
+require "facebook_graph_api/http_client" # for the ResponseError class
 
 RSpec.describe UserName do
   describe "#name_for" do
@@ -11,7 +12,7 @@ RSpec.describe UserName do
       api_builder = class_double("FacebookGraphApi::UserApi", for_user: api)
       user = instance_double("LoginSession::User")
 
-      service = described_class.new(api_builder:, user:)
+      service = described_class.new(api_builder:, user:, error_reporter: double)
       service.name_for(double)
 
       expect(api_builder).to have_received(:for_user).with(user)
@@ -23,7 +24,7 @@ RSpec.describe UserName do
       api_builder = class_double("FacebookGraphApi::UserApi", for_user: api)
       user = instance_double("LoginSession::User")
 
-      service = described_class.new(api_builder:, user:)
+      service = described_class.new(api_builder:, user:, error_reporter: double)
       service.name_for(123456)
 
       expect(api).to have_received(:profile).with(123456)
@@ -35,10 +36,47 @@ RSpec.describe UserName do
       api_builder = class_double("FacebookGraphApi::UserApi", for_user: api)
       user = instance_double("LoginSession::User")
 
-      service = described_class.new(api_builder:, user:)
+      service = described_class.new(api_builder:, user:, error_reporter: double)
       result = service.name_for(123456)
 
       expect(result).to eq "Willamae Ricker"
+    end
+
+    context "when the user name couldn't be retrieved" do
+      it "returns an unknown value" do # rubocop:disable RSpec/ExampleLength
+        api = instance_double("FacebookGraphApi::UserApi")
+        allow(api).to receive(:profile).and_raise(
+          FacebookGraphApi::HttpClient::ResponseError,
+          "Unsupported get request. Object with ID '123456' does not exist..."
+        )
+        api_builder = class_double("FacebookGraphApi::UserApi", for_user: api)
+        error_reporter = class_double("Rollbar", error: double)
+        user = instance_double("LoginSession::User")
+
+        service = described_class.new(api_builder:, user:, error_reporter:)
+        result = service.name_for(123456)
+
+        expect(result).to eq "[Unknown user]"
+      end
+
+      it "reports the error" do # rubocop:disable RSpec/ExampleLength
+        api = instance_double("FacebookGraphApi::UserApi")
+        allow(api).to receive(:profile).and_raise(
+          FacebookGraphApi::HttpClient::ResponseError,
+          "Unsupported get request. Object with ID '123456' does not exist..."
+        )
+        api_builder = class_double("FacebookGraphApi::UserApi", for_user: api)
+        error_reporter = class_double("Rollbar", error: double)
+        user = instance_double("LoginSession::User")
+
+        service = described_class.new(api_builder:, user:, error_reporter:)
+        service.name_for(123456)
+
+        expect(error_reporter).to have_received(:error).with(
+          an_instance_of(FacebookGraphApi::HttpClient::ResponseError)
+            .and(have_attributes(message: "Unsupported get request. Object with ID '123456' does not exist..."))
+        )
+      end
     end
   end
 end
