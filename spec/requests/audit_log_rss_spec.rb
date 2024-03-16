fc88d253ebbf6ba62b41af195b97ec34dce1d4a5
Duncan Stuart <dgmstuart@gmail.com>
2024-03-16 17:57:32 +0100

    RSS Audit log - add a proper spec
    This is hard, because there are a ton of ids and timestamps.

--- spec/requests/audit_log_rss_spec.rb
+++ spec/requests/audit_log_rss_spec.rb
@@ -11,11 +11,12 @@ RSpec.describe "Audit Log RSS feed" do
     venue.update!(name: "La Mesquita")
     venue.destroy!
 
-    event = create(:event, title: "The Wednesday Stomp")
-    event.update!(title: "The Tuesday Stamp")
-    event.destroy!
+    social_dance = create(:social, title: "The Wednesday Stomp", url: "https://stomp.uk")
+    social_dance.update!(title: "The Tuesday Stamp")
+    social_dance.destroy!
 
-    create(:class)
+    class_organiser = create(:organiser, name: "Maggie McMillan")
+    dance_class = create(:class, url: "https://weds", class_organiser:)
 
     organiser = create(:organiser, name: "Johnny Waters")
     organiser.update!(name: "Jimmy Fire")
@@ -25,6 +26,33 @@ RSpec.describe "Audit Log RSS feed" do
       get "/audit_log.atom?password=pass"
     end
 
-    expect(response.media_type).to eq("application/atom+xml")
+    # rubocop:disable Layout/MultilineMethodCallIndentation
+    aggregate_failures do
+      expect(response.media_type).to eq("application/atom+xml")
+      expected_xml = File.read(fixture_file("audit_log.xml"))
+        .gsub("{{venue_id}}", venue.id.to_s)
+        .gsub("{{social_dance_id}}", social_dance.id.to_s)
+        .gsub("{{social_dance_venue_id}}", social_dance.venue.id.to_s)
+        .gsub("{{dance_class_id}}", dance_class.id.to_s)
+        .gsub("{{dance_class_organiser_id}}", class_organiser.id.to_s)
+        .gsub("{{dance_class_venue_id}}", dance_class.venue.id.to_s)
+        .gsub("{{organiser_id}}", organiser.id.to_s)
+        .chomp # chomp to remove trailing newline
+
+      normalised_body = response.body
+        .gsub(%r{<updated>.+</updated>}, "<updated>{{updated_timestamp}}</updated>")
+        .gsub(%r{<dc:date>.+</dc:date>}, "<dc:date>{{updated_timestamp}}</dc:date>")
+        .gsub(/updated_at=\d+/, "updated_at={{updated_seconds}}")
+        .gsub(/created_at&quot;: &quot;.*&quot;/, "created_at&quot;: &quot;{{updated_timestamp}}&quot;")
+        .gsub(/day&quot;: &quot;.*&quot;/, "day&quot;: &quot;{{dance_class_day}}&quot;")
+      expect(normalised_body).to eq(expected_xml)
+    end
+    # rubocop:enable Layout/MultilineMethodCallIndentation
+  end
+
+  private
+
+  def fixture_file(filename)
+    Rails.root.join("spec", "fixtures", filename)
   end
 end
