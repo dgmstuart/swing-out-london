03295e816bb37ca00ae28a7b76fa2df524e0d6f1
Duncan Stuart <dgmstuart@gmail.com>
2024-04-08 16:23:49 +0200

    Protect against DNS rebinding attacks
    Only allow traffic to the canonical host:
    
      https://prathamesh.tech/2019/09/02/dns-rebinding-attacks-protection-in-rails-6/
      https://guides.rubyonrails.org/configuring.html#actiondispatch-hostauthorization

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,6 +78,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # Enable DNS rebinding protection and other `Host` header attacks.
+  config.hosts << ENV["CANONICAL_HOST"] if ENV["CANONICAL_HOST"]
   # config.hosts = [
   #   "example.com",     # Allow requests from example.com
   #   /.*\.example\.com/ # Allow requests from subdomains like `www.example.com`
