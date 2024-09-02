b0b8c5ce749644cc7496acf71afb80651b0492b5
Duncan Stuart <dgmstuart@gmail.com>
2024-09-03 01:34:26 +0200

    Move secret_key_base to credentials
    ...for dev and test.
    
    secrets.yml is deprecated and will be removed in Rails 7.2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,4 +85,6 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
+  config.secret_key_base = ENV.fetch("SECRET_KEY_BASE")
 end
