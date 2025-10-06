ff0413559455ecd9f12be82b8e2da8af970b5b6e
Duncan Stuart <dgmstuart@gmail.com>
2025-10-06 20:03:41 +0200

    WIP hiatus description

--- spec/system/editors_can_put_events_on_hiatus_spec.rb
+++ spec/system/editors_can_put_events_on_hiatus_spec.rb
@@ -9,23 +9,23 @@ RSpec.describe "Editors can put events on hiatus", :js do
 
       skip_login
 
-      click_on "Edit", match: :first
+      Timecop.freeze(Date.parse("2012-06-01")) do
+        click_on "Edit", match: :first
 
-      choose "This event is taking a break"
-      fill_in "Start of break", with: "2010-06-28"
-      fill_in "First date back after the break", with: "2010-08-30"
+        choose "This event is taking a break"
+        fill_in "Start of break", with: "28-06-2012"
+        fill_in "First date back after the break", with: "30-08-2012"
 
-      Timecop.freeze(Date.parse("2010-06-01")) do
         click_on "Update"
 
         expect(page).to have_content("Event was successfully updated")
+
+        expect(page).to have_content("On hiatus:\nFrom 28/06/2010, returning 30/08/2010")
+
+        visit("/")
+
+        save_and_open_page
       end
-
-      expect(page).to have_content("On hiatus:\nFrom 28/06/2010, returning 30/08/2010")
-
-      visit("/")
-
-      save_and_open_page
     end
   end
 end
