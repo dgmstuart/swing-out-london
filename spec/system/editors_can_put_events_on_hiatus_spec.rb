e023a770b262c0857d79d040afadf707675c7303
Duncan Stuart <dgmstuart@gmail.com>
2025-10-10 02:22:09 +0200

    Weekly socials on hiatus don't show up in listings
    Filter out events which are on hiatus
    
    According to AI, this NOT EXISTS approach is equivalently performant to
    a JOIN and easier to reason about.
    
    We need CAST(:date AS date) because:
    
      - When working with raw SQL, Rails just passes bind params as a string
      - Postgres @> is overloaded: there are many different comparisons of
        different types, and it can’t infer the type.
      - Without the cast, it defaults to assuming we meant "range contains
        range", so tries to interpret the date string as a range (we get
        "ERROR: malformed range literal)

--- spec/system/editors_can_put_events_on_hiatus_spec.rb
+++ spec/system/editors_can_put_events_on_hiatus_spec.rb
@@ -5,7 +5,7 @@ require "rails_helper"
 RSpec.describe "Editors can put events on hiatus", :js do
   context "when the event is a weekly social" do
     it "with a valid date range" do
-      create(:weekly_social, day: "Monday")
+      create(:weekly_social, day: "Monday", title: "Camberwell Bounce")
 
       skip_login
 
@@ -25,6 +25,30 @@ RSpec.describe "Editors can put events on hiatus", :js do
         expect(page).to have_checked_field("event[status]", with: "taking_a_break")
         expect(page).to have_field("Start of break", with: "2012-06-25")
         expect(page).to have_field("First date back after the break", with: "2012-08-20")
+
+        visit("/")
+
+        expect(page).to have_content("Camberwell Bounce").twice
+      end
+
+      Timecop.freeze(Date.parse("2012-06-17")) do
+        visit("/")
+
+        expect(page).to have_content("Camberwell Bounce").once # 25th Jun isn't shown
+        expect(page).to have_content("Monday 18th June").once
+      end
+
+      Timecop.freeze(Date.parse("2012-07-01")) do
+        visit("/")
+
+        expect(page).not_to have_content("Camberwell Bounce").once # no dates in July
+      end
+
+      Timecop.freeze(Date.parse("2012-08-10")) do
+        visit("/")
+
+        expect(page).to have_content("Camberwell Bounce").once # 13th Aug isn't shown
+        expect(page).to have_content("Monday 20th August").once
       end
     end
   end
