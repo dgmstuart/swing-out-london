06612a61b185b66ac04af0109811fc711f417c2a
Duncan Stuart <dgmstuart@gmail.com>
2026-01-08 16:52:31 +0100

    Attempt to block more bots
    Try to expand the possibility of triggering fail2ban

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -10,10 +10,12 @@ Rack::Attack.blocklist("block dodgy IP addresses") do |request|
   ip_blocklist.include? request.ip
 end
 
-WORDPRESS_PATHS = %w[wp-login wp-admin wp-includes wp-content xmlrpc.php wordpress].freeze
-OTHER_PROGRAMMING_LANGUAGES = %w[.php .asp].freeze
+WORDPRESS_PATHS = %w[wp-login wp-admin wp-includes wp-content xmlrpc wordpress].freeze
+OTHER_PROGRAMMING_LANGUAGES = %w[php .asp].freeze
 MALWARE_PATHS = %w[alfacgiapi ALFA_DATA cgialfa].freeze
-SUSPICIOUS_PATHS = WORDPRESS_PATHS + OTHER_PROGRAMMING_LANGUAGES + MALWARE_PATHS
+GIT_PATHS = %w[git-secret gitignore gitlab].freeze
+DIRECTORY_TRAVERSAL_PATHS = %w[backup backend credentials].freeze
+SUSPICIOUS_PATHS = WORDPRESS_PATHS + OTHER_PROGRAMMING_LANGUAGES + MALWARE_PATHS + GIT_PATHS + DIRECTORY_TRAVERSAL_PATHS
 # If we receive 3 requests which look like attempted hacks, in the span of 10 mins, ban that IP for 15 mins
 Rack::Attack.blocklist("Block suspicious requests") do |request|
   Rack::Attack::Fail2Ban.filter("suspicious-#{request.ip}", maxretry: 3, findtime: 10.minutes, bantime: 15.minutes) do
